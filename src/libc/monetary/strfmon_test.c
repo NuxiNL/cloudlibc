35395c91364aaa8081cf670d024007caf8e4c997
Ed Schouten <ed@nuxi.nl>
2015-03-24 19:23:52 +0100

    Don't use negative numbers in the tests.
    These have been copied from the POSIX examples incorrectly.

--- src/libc/monetary/strfmon_test.c
+++ src/libc/monetary/strfmon_test.c
@@ -17,84 +17,84 @@ TEST(strfmon, posix_examples) {
   ASSERT_STREQ("$123.45", buf);
   ASSERT_EQ(8, strfmon_l(buf, sizeof(buf), locale, "%n", -123.45));
   ASSERT_STREQ("-$123.45", buf);
-  ASSERT_EQ(9, strfmon_l(buf, sizeof(buf), locale, "%n", -3456.781));
+  ASSERT_EQ(9, strfmon_l(buf, sizeof(buf), locale, "%n", 3456.781));
   ASSERT_STREQ("$3,456.78", buf);
 
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%11n", 123.45));
   ASSERT_STREQ("    $123.45", buf);
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%11n", -123.45));
   ASSERT_STREQ("   -$123.45", buf);
-  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%11n", -3456.781));
+  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%11n", 3456.781));
   ASSERT_STREQ("  $3,456.78", buf);
 
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%#5n", 123.45));
   ASSERT_STREQ(" $   123.45", buf);
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%#5n", -123.45));
   ASSERT_STREQ("-$   123.45", buf);
-  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%#5n", -3456.781));
+  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%#5n", 3456.781));
   ASSERT_STREQ(" $ 3,456.78", buf);
 
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%=*#5n", 123.45));
   ASSERT_STREQ(" $***123.45", buf);
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%=*#5n", -123.45));
   ASSERT_STREQ("-$***123.45", buf);
-  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%=*#5n", -3456.781));
+  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%=*#5n", 3456.781));
   ASSERT_STREQ(" $*3,456.78", buf);
 
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%=0#5n", 123.45));
   ASSERT_STREQ(" $000123.45", buf);
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%=*#5n", -123.45));
   ASSERT_STREQ("-$000123.45", buf);
-  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%=*#5n", -3456.781));
+  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%=*#5n", 3456.781));
   ASSERT_STREQ(" $03,456.78", buf);
 
   ASSERT_EQ(10, strfmon_l(buf, sizeof(buf), locale, "%^#5n", 123.45));
   ASSERT_STREQ(" $  123.45", buf);
   ASSERT_EQ(10, strfmon_l(buf, sizeof(buf), locale, "%^#5n", -123.45));
   ASSERT_STREQ("-$  123.45", buf);
-  ASSERT_EQ(10, strfmon_l(buf, sizeof(buf), locale, "%^#5n", -3456.781));
+  ASSERT_EQ(10, strfmon_l(buf, sizeof(buf), locale, "%^#5n", 3456.781));
   ASSERT_STREQ(" $ 3456.78", buf);
 
   ASSERT_EQ(7, strfmon_l(buf, sizeof(buf), locale, "%^#5.0n", 123.45));
   ASSERT_STREQ(" $  123", buf);
   ASSERT_EQ(7, strfmon_l(buf, sizeof(buf), locale, "%^#5.0n", -123.45));
   ASSERT_STREQ("-$  123", buf);
-  ASSERT_EQ(7, strfmon_l(buf, sizeof(buf), locale, "%^#5.0n", -3456.781));
+  ASSERT_EQ(7, strfmon_l(buf, sizeof(buf), locale, "%^#5.0n", 3456.781));
   ASSERT_STREQ(" $ 3457", buf);
 
   ASSERT_EQ(12, strfmon_l(buf, sizeof(buf), locale, "%^#5.4n", 123.45));
   ASSERT_STREQ(" $  123.4500", buf);
   ASSERT_EQ(12, strfmon_l(buf, sizeof(buf), locale, "%^#5.4n", -123.45));
   ASSERT_STREQ("-$  123.4500", buf);
-  ASSERT_EQ(12, strfmon_l(buf, sizeof(buf), locale, "%^#5.4n", -3456.781));
+  ASSERT_EQ(12, strfmon_l(buf, sizeof(buf), locale, "%^#5.4n", 3456.781));
   ASSERT_STREQ(" $ 3456.7810", buf);
 
   ASSERT_EQ(12, strfmon_l(buf, sizeof(buf), locale, "%(#5n", 123.45));
   ASSERT_STREQ(" $   123.45 ", buf);
   ASSERT_EQ(12, strfmon_l(buf, sizeof(buf), locale, "%(#5n", -123.45));
   ASSERT_STREQ("($   123.45)", buf);
-  ASSERT_EQ(12, strfmon_l(buf, sizeof(buf), locale, "%(#5n", -3456.781));
+  ASSERT_EQ(12, strfmon_l(buf, sizeof(buf), locale, "%(#5n", 3456.781));
   ASSERT_STREQ(" $ 3,456.78 ", buf);
 
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%!(#5n", 123.45));
   ASSERT_STREQ("    123.45 ", buf);
   ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%!(#5n", -123.45));
   ASSERT_STREQ("(   123.45)", buf);
-  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%!(#5n", -3456.781));
+  ASSERT_EQ(11, strfmon_l(buf, sizeof(buf), locale, "%!(#5n", 3456.781));
   ASSERT_STREQ("  3,456.78 ", buf);
 
   ASSERT_EQ(14, strfmon_l(buf, sizeof(buf), locale, "%-14#5.4n", 123.45));
   ASSERT_STREQ(" $   123.4500 ", buf);
   ASSERT_EQ(14, strfmon_l(buf, sizeof(buf), locale, "%-14#5.4n", -123.45));
   ASSERT_STREQ("-$   123.4500 ", buf);
-  ASSERT_EQ(14, strfmon_l(buf, sizeof(buf), locale, "%-14#5.4n", -3456.781));
+  ASSERT_EQ(14, strfmon_l(buf, sizeof(buf), locale, "%-14#5.4n", 3456.781));
   ASSERT_STREQ(" $ 3,456.7810 ", buf);
 
   ASSERT_EQ(14, strfmon_l(buf, sizeof(buf), locale, "%14#5.4n", 123.45));
   ASSERT_STREQ("  $   123.4500", buf);
   ASSERT_EQ(14, strfmon_l(buf, sizeof(buf), locale, "%14#5.4n", -123.45));
   ASSERT_STREQ(" -$   123.4500", buf);
-  ASSERT_EQ(14, strfmon_l(buf, sizeof(buf), locale, "%14#5.4n", -3456.781));
+  ASSERT_EQ(14, strfmon_l(buf, sizeof(buf), locale, "%14#5.4n", 3456.781));
   ASSERT_STREQ("  $ 3,456.7810", buf);
 
   freelocale(locale);
