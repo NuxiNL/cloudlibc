9d4b5bb5f814c49e2eaa2a2901bc89f65089a3aa
Ed Schouten <ed@nuxi.nl>
2015-03-24 14:49:06 +0100

    Add the basic state machine for strfmon().

--- src/common/vstrfmon.h
+++ src/common/vstrfmon.h
@@ -5,7 +5,9 @@
 
 #include <sys/types.h>
 
+#include <errno.h>
 #include <stdarg.h>
+#include <stdbool.h>
 
 #if WIDE
 #include <wchar.h>
@@ -17,6 +19,80 @@ typedef char char_t;
 
 ssize_t NAME(char_t *restrict s, size_t maxsize, locale_t locale,
              const char_t *restrict format, va_list ap) {
-  // TODO(edje): Implement.
-  return -1;
+  size_t nwritten = 0;
+#define PUTCHAR(c)             \
+  do {                         \
+    if (nwritten >= maxsize) { \
+      errno = E2BIG;           \
+      return -1;               \
+    }                          \
+    s[nwritten++] = (c);       \
+  } while (0)
+
+  while (*format != '\0') {
+    if (*format == '%') {
+      ++format;
+
+      // Parse flags.
+      char_t fill_character = ' ';
+      bool grouping = true, parentheses = false, currency_symbol = true,
+           left_justified = false;
+      for (;;) {
+        if (*format == '=') {
+          fill_character = *++format;
+        } else if (*format == '^') {
+          grouping = false;
+        } else if (*format == '+') {
+          parentheses = false;
+        } else if (*format == '(') {
+          parentheses = true;
+        } else if (*format == '!') {
+          currency_symbol = false;
+        } else if (*format == '-') {
+          left_justified = true;
+        } else {
+          break;
+        }
+        ++format;
+      }
+
+      // Field width.
+      unsigned int field_width = 0;
+      while (*format >= '0' && *format <= '9')
+        field_width = field_width * 10 + *format++ - '0';
+
+      // Left precision.
+      unsigned int left_precision = 0;
+      if (*format == '#') {
+        ++format;
+        while (*format >= '0' && *format <= '9')
+          left_precision = left_precision * 10 + *format++ - '0';
+      }
+
+      // Right precision.
+      unsigned int right_precision;
+      if (*format == '.') {
+        ++format;
+        right_precision = 0;
+        while (*format >= '0' && *format <= '9')
+          right_precision = right_precision * 10 + *format++ - '0';
+      } else {
+        // TODO(edje): Get default value from locale.
+        right_precision = 2;
+      }
+
+      if (*format == 'i' || *format == 'n') {
+        // TODO(edje): Implement.
+      } else if (*format == '%') {
+        PUTCHAR('%');
+      }
+    } else {
+      PUTCHAR(*format);
+    }
+    ++format;
+  }
+
+  // Add trailing null byte.
+  PUTCHAR('\0');
+  return nwritten - 1;
 }
