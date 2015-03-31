755289c0aacbd5adb2a994718005d5c6ef881ad0
Ed Schouten <ed@nuxi.nl>
2015-03-31 09:45:20 +0200

    Add boilerplate for dladdr().
    As we only support static linkage, the standard functions provided by
    dlfcn.h are not useful. dladdr(), however, is useful, as it is an
    important programming interface for debugging. It can be used to
    generate backtraces, for example.

---
+++ src/include/dlfcn.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
+// 1. Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+// SUCH DAMAGE.
+
+// <dlfcn.h> - dynamic linking
+//
+// Extensions:
+// - dladdr():
+//   Present on most operating systems. Useful for debugging.
+//
+// Features missing:
+// - RTLD_*, dlclose(), dlerror(), dlopen() and dlsym():
+//   Loading shared libraries is not supported.
+
+#ifndef _DLFCN_H_
+#define _DLFCN_H_
+
+#include <_/cdefs.h>
+
+typedef struct dl_info {
+  const char *dli_fname;  // Pathname of shared object.
+  void *dli_fbase;        // Base address of shared object.
+  const char *dli_sname;  // Name of nearest symbol.
+  void *dli_saddr;        // Address of nearest symbol.
+} Dl_info;
+
+__BEGIN_DECLS
+int dladdr(const void *, Dl_info *);
+__END_DECLS
+
+#endif
---
+++ src/libc/dlfcn/dladdr.c
@@ -0,0 +1,11 @@
+// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+//
+// This file is distrbuted under a 2-clause BSD license.
+// See the LICENSE file for details.
+
+#include <dlfcn.h>
+
+int dladdr(const void *addr, Dl_info *info) {
+  // TODO(edje): Implement.
+  return 0;
+}
