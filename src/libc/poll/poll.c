2bd3847d336988687f31b9caf9e74876ffc19638
Ed Schouten <ed@nuxi.nl>
2015-04-07 21:03:27 +0200

    Make the poll() code a bit more complete.
    Now it's a matter of extending poll_once() to be able to support this.

--- src/libc/poll/poll.c
+++ src/libc/poll/poll.c
@@ -9,7 +9,7 @@
 #include <poll.h>
 #include <stddef.h>
 
-int poll(struct pollfd fds[], size_t nfds, int timeout) {
+int poll(struct pollfd *fds, size_t nfds, int timeout) {
   size_t maxevents = 2 * nfds + 1;
   cloudabi_event_t events[2 * nfds + 1];
   size_t nevents = 0;
@@ -17,6 +17,8 @@ int poll(struct pollfd fds[], size_t nfds, int timeout) {
   // Construct events for poll_once().
   for (size_t i = 0; i < nfds; ++i) {
     struct pollfd *pollfd = &fds[i];
+    if (pollfd->fd < 0)
+      continue;
     if ((pollfd->events & POLLRDNORM) != 0) {
       cloudabi_event_t *event = &events[nevents++];
       event->userdata = pollfd;
@@ -29,14 +31,17 @@ int poll(struct pollfd fds[], size_t nfds, int timeout) {
       event->type = CLOUDABI_EVENT_TYPE_FD_WRITE;
       event->fd_readwrite.fd = pollfd->fd;
     }
-    pollfd->revents = 0;
   }
 
   // Create extra event for the timeout.
-  if (timeout != -1) {
+  if (timeout >= 0) {
     cloudabi_event_t *event = &events[nevents++];
     event->type = CLOUDABI_EVENT_TYPE_CLOCK;
-    // TODO(edje): Set fields.
+    event->clock.timeout = 0;
+    cloudabi_sys_clock_time_get(CLOUDABI_CLOCK_REALTIME, 0,
+                                &event->clock.timeout);
+    event->clock.timeout += (cloudabi_timestamp_t)timeout * 1000000;
+    event->clock.precision = 0;
   }
 
   // Execute poll_once().
@@ -47,9 +52,33 @@ int poll(struct pollfd fds[], size_t nfds, int timeout) {
     return -1;
   }
 
+  // Clear revents fields.
+  for (size_t i = 0; i < nfds; ++i) {
+    struct pollfd *pollfd = &fds[i];
+    pollfd->revents = 0;
+  }
+
   // Set revents fields.
   for (size_t i = 0; i < nevents; ++i) {
-    // TODO(edje): Implement.
+    const cloudabi_event_t *event = &events[i];
+    if (event->type == CLOUDABI_EVENT_TYPE_FD_READ ||
+        event->type == CLOUDABI_EVENT_TYPE_FD_WRITE) {
+      struct pollfd *pollfd = event->userdata;
+      // TODO(edje): Add support for POLLHUP.
+      if (event->error == EBADF) {
+        // Invalid file descriptor.
+        pollfd->revents |= POLLNVAL;
+      } else if (pollfd->revents != 0) {
+        // Another error occurred.
+        pollfd->revents |= POLLERR;
+      } else if (event->type == CLOUDABI_EVENT_TYPE_FD_READ) {
+        // Data can be read.
+        pollfd->revents |= POLLRDNORM;
+      } else {
+        // Data can be written.
+        pollfd->revents |= POLLWRNORM;
+      }
+    }
   }
 
   return 0;
