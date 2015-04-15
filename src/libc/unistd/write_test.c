4a717988aa611d860579ba3008fcc47c4a1e1701
Ed Schouten <ed@nuxi.nl>
2015-04-15 10:41:07 +0200

    Add tests to validate read() and write() on kqueues.

--- src/libc/unistd/write_test.c
+++ src/libc/unistd/write_test.c
@@ -3,15 +3,24 @@
 // This file is distrbuted under a 2-clause BSD license.
 // See the LICENSE file for details.
 
+#include <sys/event.h>
+
 #include <errno.h>
 #include <testing.h>
 #include <unistd.h>
 
 TEST(write, bad) {
-  // Invalid file descriptor.
+  // Bad file descriptor.
   char b;
   ASSERT_EQ(-1, write(0xdeadc0de, &b, 1));
   ASSERT_EQ(EBADF, errno);
+
+  // Invalid file descriptor type.
+  int fd = kqueue();
+  ASSERT_LE(0, fd);
+  ASSERT_EQ(-1, write(fd, &b, 1));
+  ASSERT_EQ(EBADF, errno);
+  ASSERT_EQ(0, close(fd));
 }
 
 TEST(write, example) {
