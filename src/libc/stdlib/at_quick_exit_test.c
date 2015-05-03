31027f42c148cc8d0f309b505406f683732988df
Ed Schouten <ed@nuxi.nl>
2015-05-03 17:12:01 +0200

    Add tests for quick_exit() and at_quick_exit().

---
+++ src/libc/stdlib/at_quick_exit_test.c
@@ -0,0 +1,52 @@
+// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+//
+// This file is distrbuted under a 2-clause BSD license.
+// See the LICENSE file for details.
+
+#include <sys/procdesc.h>
+
+#include <signal.h>
+#include <stdlib.h>
+#include <testing.h>
+#include <unistd.h>
+
+static int val = 1;
+
+static void handler1(void) {
+  ASSERT_EQ(1, val);
+  val = 2;
+}
+
+static void handler2(void) {
+  ASSERT_EQ(2, val);
+  val = 3;
+}
+
+static void handler3(void) {
+  ASSERT_EQ(3, val);
+  _Exit(86);
+}
+
+TEST(at_quick_exit, example) {
+  // Fork process.
+  int fd;
+  int ret = pdfork(&fd);
+
+  // Terminate the child process.
+  if (ret == 0) {
+    ASSERT_EQ(0, at_quick_exit(handler3));
+    ASSERT_EQ(0, at_quick_exit(handler2));
+    ASSERT_EQ(0, at_quick_exit(handler1));
+    quick_exit(43);
+  }
+
+  ASSERT_LT(0, ret);
+  siginfo_t si;
+  ASSERT_EQ(0, pdwait(fd, &si, 0));
+  ASSERT_EQ(0, close(fd));
+
+  // Validate exit code.
+  ASSERT_EQ(SIGCHLD, si.si_signo);
+  ASSERT_EQ(CLD_EXITED, si.si_code);
+  ASSERT_EQ(86, si.si_status);
+}
