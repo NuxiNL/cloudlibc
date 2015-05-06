9d7d273c6f60fbc425f82d5b7b5d8157d1ba5413
Ed Schouten <ed@nuxi.nl>
2015-05-06 12:54:53 +0200

    Also add a sleep to the send() unit test to propagate the disconnect.

--- src/libc/sys/socket/send_test.c
+++ src/libc/sys/socket/send_test.c
@@ -8,6 +8,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <testing.h>
+#include <time.h>
 #include <unistd.h>
 
 TEST(send, bad) {
@@ -34,7 +35,12 @@ TEST(send, bad) {
   ASSERT_EQ(0, socketpair(AF_UNIX, SOCK_STREAM, 0, fds));
   ASSERT_EQ(-1, send(fds[0], &b, 1, 0xdeadc0de));
   ASSERT_EQ(EOPNOTSUPP, errno);
+
+  // Close one side and wait for the other side to switch to the
+  // disconnected state.
   ASSERT_EQ(0, close(fds[1]));
+  ASSERT_EQ(0, clock_nanosleep(CLOCK_MONOTONIC, 0,
+                               &(struct timespec){.tv_nsec = 100000000L}));
 
   // Connection has been closed.
   ASSERT_EQ(-1, send(fds[0], &b, 1, 0));
