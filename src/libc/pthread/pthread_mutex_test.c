fdd33af8bdbd69f15d94ee9c5110be89d5bb2d1d
Ed Schouten <ed@nuxi.nl>
2015-05-06 11:32:49 +0200

    Let one of the pthread shared memory tests use a backing file.
    Instead of just testing private and shared anonymous memory, also make
    sure we test mutexes placed on top of actual file objects. This allows
    me to test NetBSD properly, as anonymous memory is implemented outside
    of vmobjects.

--- src/libc/pthread/pthread_mutex_test.c
+++ src/libc/pthread/pthread_mutex_test.c
@@ -7,6 +7,7 @@
 #include <sys/procdesc.h>
 
 #include <errno.h>
+#include <fcntl.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <testing.h>
@@ -48,10 +49,15 @@ TEST(pthread_mutex, private) __no_lock_analysis {
 }
 
 TEST(pthread_mutex, shared) __no_lock_analysis {
-  // Allocate a piece of shared memory to store the mutex.
-  pthread_mutex_t *mutex = mmap(NULL, sizeof(*mutex), PROT_READ | PROT_WRITE,
-                                MAP_SHARED | MAP_ANON, -1, 0);
+  // Create a temporary file to use as shared memory to store the mutex.
+  int fd = openat(fd_tmp, "tmp", O_CREAT | O_RDWR);
+  ASSERT_EQ(0, ftruncate(fd, sizeof(pthread_mutex_t)));
+  pthread_mutex_t *mutex =
+      mmap(NULL, sizeof(*mutex), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  ASSERT_EQ(0, close(fd));
   ASSERT_NE(MAP_FAILED, mutex);
+
+  // Create shared mutex.
   pthread_mutexattr_t attr;
   ASSERT_EQ(0, pthread_mutexattr_init(&attr));
   ASSERT_EQ(0, pthread_mutexattr_setpshared(&attr, PTHREAD_PROCESS_SHARED));
@@ -65,7 +71,6 @@ TEST(pthread_mutex, shared) __no_lock_analysis {
   ASSERT_EQ(0, pthread_mutex_unlock(mutex));
 
   // Spawn a new process that will acquire the lock.
-  int fd;
   int ret = pdfork(&fd);
   if (ret == 0) {
     do_block(mutex);
