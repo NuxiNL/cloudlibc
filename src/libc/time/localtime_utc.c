42af2f5b6fb81a99af966ca8ab9def8fafe97f08
Ed Schouten <ed@nuxi.nl>
2015-05-07 11:19:00 +0200

    Make localtime()/gmtime() conversion run in constant time.
    Instead of iterating 400 times, we can use some integer arithmetic to
    quickly determine the year we're in.

--- src/libc/time/localtime_utc.c
+++ src/libc/time/localtime_utc.c
@@ -10,47 +10,47 @@
 
 #include "time_impl.h"
 
-static time_t modulo_remainder(time_t *numer, time_t denom) {
-  time_t rem = *numer % denom;
-  *numer /= denom;
-  if (rem < 0) {
-    rem += denom;
-    --*numer;
-  }
-  return rem;
-}
+#define ERALENGTH ((time_t)146097 * 24 * 60 * 60)
 
 int __localtime_utc(time_t timer, struct tm *result) {
-  // Compute time within the day.
-  struct tm tm = {};
-  tm.tm_sec = modulo_remainder(&timer, 60);
-  tm.tm_min = modulo_remainder(&timer, 60);
-  tm.tm_hour = modulo_remainder(&timer, 24);
-
-  // Compute weekday. Day 0 is a Thursday.
-  time_t day = timer + 4;
-  tm.tm_wday = modulo_remainder(&day, 7);
-
-  // Compute the year and the day within year by iteratively subtracting
-  // the days in a year. Limit the number of iterations by making use of
-  // the fact that the calendar repeats every 146097 days.
-  tm.tm_yday = modulo_remainder(&timer, 146097);
-  time_t year = timer * 400 + 70;
-  const short *months;
-  for (;;) {
-    months = get_months(year);
-    if (tm.tm_yday < months[12])
-      break;
-    tm.tm_yday -= months[12];
-    ++year;
+  // The calendar repeats every 146097 days (400 years). Normalize the
+  // timer to lie within this range. This ensures that the timestamp is
+  // positive, simplifying the arithmetic below.
+  time_t era = timer / ERALENGTH;
+  timer %= ERALENGTH;
+  if (timer < 0) {
+    timer += ERALENGTH;
+    --era;
   }
 
-  // We cannot represent this time using a "struct tm".
+  // Compute time within the day.
+  struct tm tm = {};
+  tm.tm_sec = timer % 60;
+  timer /= 60;
+  tm.tm_min = timer % 60;
+  timer /= 60;
+  tm.tm_hour = timer % 24;
+  timer /= 24;
+
+  // Compute weekday. Day 0 is a Thursday.
+  tm.tm_wday = (timer + 4) % 7;
+
+  // Apply leap days, so that every year becomes exactly 366 days long.
+  // First add the three leap days at the boundaries of the centuries.
+  // After that we can add up to three leap days per 1461 days.
+  timer += (timer - 11323) / 36524;
+  timer += timer / 1461 + (timer + 731) / 1461 + (timer + 1096) / 1461;
+  tm.tm_yday = timer % 366;
+
+  // Compute the year. "struct tm" only uses an integer to hold the year
+  // number. Bail out if the year number doesn't fit.
+  time_t year = era * 400 + timer / 366 + 70;
   if (year < INT_MIN || year > INT_MAX)
     return EOVERFLOW;
   tm.tm_year = year;
 
   // Compute month and day within month.
+  const short *months = get_months(year);
   while (tm.tm_yday >= months[tm.tm_mon + 1])
     ++tm.tm_mon;
   tm.tm_mday = tm.tm_yday - months[tm.tm_mon] + 1;
