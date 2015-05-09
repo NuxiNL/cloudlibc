036e8aaef4d48da6767f6ae2068ed7938d1b8a6c
Ed Schouten <ed@nuxi.nl>
2015-05-09 22:15:02 +0200

    Validate rights on a single socket.

--- src/libc/sys/socket/socket_test.c
+++ src/libc/sys/socket/socket_test.c
@@ -3,9 +3,11 @@
 // This file is distrbuted under a 2-clause BSD license.
 // See the LICENSE file for details.
 
+#include <sys/capsicum.h>
 #include <sys/socket.h>
 
 #include <errno.h>
+#include <fcntl.h>
 #include <testing.h>
 #include <unistd.h>
 
@@ -23,9 +25,21 @@ TEST(socket, bad) {
   ASSERT_EQ(EPROTONOSUPPORT, errno);
 }
 
-TEST(socket, example) {
+TEST(socket, rights) {
   // Create a new socket.
   int fd = socket(AF_UNIX, SOCK_STREAM, 0);
   ASSERT_LE(0, fd);
+
+  // Validate rights.
+  cap_rights_t base, inheriting, all;
+  CAP_ALL(&all);
+  ASSERT_EQ(0, cap_rights_get_explicit(fd, &base, &inheriting));
+  ASSERT_EQ(CAP_READ | CAP_WRITE | CAP_FCNTL | CAP_FSTAT | CAP_SOCK_CLIENT |
+                CAP_SOCK_SERVER | CAP_EVENT,
+            base.__value);
+  ASSERT_EQ(all.__value, inheriting.__value);
+
+  // Validate access modes.
+  ASSERT_EQ(O_RDWR, fcntl(fd, F_GETFL));
   ASSERT_EQ(0, close(fd));
 }
