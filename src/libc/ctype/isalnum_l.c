d0135fb3226e8dfe481a1bee2aafad4ca6320063
Ed Schouten <ed@nuxi.nl>
2015-06-24 21:25:43 +0200

    Fix style.

--- src/libc/ctype/isalnum_l.c
+++ src/libc/ctype/isalnum_l.c
@@ -3,10 +3,10 @@
 // This file is distrbuted under a 2-clause BSD license.
 // See the LICENSE file for details.
 
-#include "ctype_impl.h"
-
 #include <ctype.h>
 #include <wctype.h>
+
+#include "ctype_impl.h"
 
 int isalnum_l(int c, locale_t locale) {
   return isctype_l(c, iswalnum, locale);
