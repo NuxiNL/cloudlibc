e47164fa7b64daa7501f822083da94dd5dc46ad0
Ed Schouten <ed@nuxi.nl>
2015-06-25 20:28:12 +0200

    Simplify btowc() and wctob().
    There is no need to call into the localized version for such a simple
    operation.

--- src/libc/wchar/btowc.c
+++ src/libc/wchar/btowc.c
@@ -3,12 +3,10 @@
 // This file is distrbuted under a 2-clause BSD license.
 // See the LICENSE file for details.
 
-#include <common/locale.h>
-
-#include <locale.h>
 #include <wchar.h>
 
 wint_t btowc(int c) {
-  DEFAULT_LOCALE(locale, LC_CTYPE_MASK);
-  return btowc_l(c, locale);
+  if (c < 0 || c > 127)
+    return WEOF;
+  return c;
 }
--- src/libc/wchar/wctob.c
+++ src/libc/wchar/wctob.c
@@ -3,12 +3,11 @@
 // This file is distrbuted under a 2-clause BSD license.
 // See the LICENSE file for details.
 
-#include <common/locale.h>
-
-#include <locale.h>
+#include <stdio.h>
 #include <wchar.h>
 
 int wctob(wint_t c) {
-  DEFAULT_LOCALE(locale, LC_CTYPE_MASK);
-  return wctob_l(c, locale);
+  if (c < 0 || c > 127)
+    return EOF;
+  return c;
 }
