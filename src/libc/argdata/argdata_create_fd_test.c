814b7d146a91cd74b23865de0fb18f5ae08028c5
Ed Schouten <ed@nuxi.nl>
2015-07-04 20:17:12 +0200

    Do some random testing of create_fd() as well.

---
+++ src/libc/argdata/argdata_create_fd_test.c
@@ -0,0 +1,32 @@
+// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+//
+// This file is distrbuted under a 2-clause BSD license.
+// See the LICENSE file for details.
+
+#include <argdata.h>
+#include <errno.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <testing.h>
+
+TEST(argdata_create_fd, bad) {
+  // Bad file descriptors.
+  ASSERT_EQ(NULL, argdata_create_fd(-1));
+  ASSERT_EQ(EBADF, errno);
+  ASSERT_EQ(NULL, argdata_create_fd(INT_MIN));
+  ASSERT_EQ(EBADF, errno);
+}
+
+TEST(argdata_create_int, signed) {
+  for (size_t i = 0; i < 1000; ++i) {
+    // Create new file descriptor object.
+    int j = arc4random_uniform(INT_MAX);
+    argdata_t *ad = argdata_create_fd(j);
+
+    // Read the value back in.
+    int k;
+    ASSERT_EQ(0, argdata_get_fd(ad, &k));
+    ASSERT_EQ(j, k);
+    argdata_free(ad);
+  }
+}
