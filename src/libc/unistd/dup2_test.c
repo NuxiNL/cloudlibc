c1a64c38bad8167a4222fc800fad535c003799cf
Ed Schouten <ed@nuxi.nl>
2015-07-09 13:49:23 +0200

    Expand testing coverage for dup2().

--- src/libc/unistd/dup2_test.c
+++ src/libc/unistd/dup2_test.c
@@ -5,14 +5,35 @@
 
 #include <sys/stat.h>
 
+#include <errno.h>
 #include <fcntl.h>
 #include <testing.h>
 #include <unistd.h>
 
-TEST(dup2, file) {
+TEST_SEPARATE_PROCESS(dup2, bad) {
+  // Source file descriptor is invalid.
+  ASSERT_EQ(-1, dup2(-1, fd_tmp));
+  ASSERT_EQ(EBADF, errno);
+
+  // Target file descriptor is invalid.
+  ASSERT_EQ(-1, dup2(fd_tmp, -1));
+  ASSERT_EQ(EBADF, errno);
+
+  // It is not possible to create new file descriptors using dup2(), for
+  // the reason that file descriptor order does not matter in this
+  // environment. It is also inherently thread-unsafe.
+  int fds[2];
+  ASSERT_EQ(0, pipe(fds));
+  ASSERT_EQ(0, close(fds[1]));
+  ASSERT_EQ(-1, dup2(fds[0], fds[1]));
+  ASSERT_EQ(EBADF, errno);
+  ASSERT_EQ(0, close(fds[0]));
+}
+
+TEST(dup2, good) {
+  // Create two files for testing.
   int fd1 = openat(fd_tmp, "file1", O_CREAT | O_EXCL | O_WRONLY);
   ASSERT_LE(0, fd1);
-
   int fd2 = openat(fd_tmp, "file2", O_CREAT | O_EXCL | O_WRONLY);
   ASSERT_LE(0, fd2);
   ASSERT_NE(fd1, fd2);
