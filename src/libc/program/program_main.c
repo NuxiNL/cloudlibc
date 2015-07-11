9c5f341a9e08bdd87620d0951c43bf11880b0476
Ed Schouten <ed@nuxi.nl>
2015-07-11 23:14:52 +0200

    Add support for command line arguments to the fallback program_main().
    If a program is not spawned using cloudabi-run, it can still decipher
    the arguments that are passed to it.

--- src/libc/program/program_main.c
+++ src/libc/program/program_main.c
@@ -3,19 +3,37 @@
 // This file is distrbuted under a 2-clause BSD license.
 // See the LICENSE file for details.
 
-#include <argdata.h>
+#include <common/argdata.h>
+
 #include <program.h>
 #include <stdlib.h>
 #include <stdnoreturn.h>
+#include <string.h>
 
 int main(int, char **, char **);
 
+// Fallback for when the program does not contain program_main(), so
+// that it can call into the C standard main() function.
 noreturn void program_main(const argdata_t *ad) {
-  // In case the program does not provide program_main(), attempt to
-  // call into the C standard main() function. Provide at least argv[0]
-  // to prevent existing (broken) applications from crashing.
-  char program[] = "program";
-  char *argv[2] = {program, NULL};
+  // Copy the argument data over to a writable null-terminated buffer.
+  char arg[ad->length + 1];
+  memcpy(arg, ad->buffer, ad->length);
+  arg[ad->length] = '\0';
+
+  // Count the number of arguments.
+  size_t argc = 0;
+  for (size_t i = 0; i <= ad->length; ++i)
+    if (arg[i] == '\0')
+      ++argc;
+
+  // Fill argv array with pointers to individual strings.
+  char *argv[argc + 1];
+  char *p = arg;
+  for (size_t i = 0; i < argc; ++i) {
+    argv[i] = p;
+    p += strlen(p) + 1;
+  }
+  argv[argc] = NULL;
   char *env = NULL;
-  exit(main(1, argv, &env));
+  exit(main(argc, argv, &env));
 }
