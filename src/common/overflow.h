f8e671afb3a262dd88964fb35e4d01c6076380ef
Ed Schouten <ed@nuxi.nl>
2015-08-25 10:22:37 +0200

    Make <common/overflow.h> work for unsigned shorts.

--- src/common/overflow.h
+++ src/common/overflow.h
@@ -6,12 +6,35 @@
 #ifndef COMMON_OVERFLOW_H
 #define COMMON_OVERFLOW_H
 
+#include <limits.h>
+#include <stdbool.h>
+
+// Implementations for types for which no intrinsics exist.
+static inline bool uaddh_overflow(unsigned short a, unsigned short b,
+                                  unsigned short *c) {
+  *c = a + b;
+  return a > USHRT_MAX - b;
+}
+
+static inline bool usubh_overflow(unsigned short a, unsigned short b,
+                                  unsigned short *c) {
+  *c = a - b;
+  return a < b;
+}
+
+static inline bool umulh_overflow(unsigned short a, unsigned short b,
+                                  unsigned short *c) {
+  *c = a * b;
+  return b != 0 && a > USHRT_MAX / b;
+}
+
 // clang-format off
 #define COMPUTE_OVERFLOW(op, x, y, out)                 \
   _Generic(*(out),                                      \
            int: __builtin_s##op##_overflow,             \
            long: __builtin_s##op##l_overflow,           \
            long long: __builtin_s##op##ll_overflow,     \
+           unsigned short: u##op##h_overflow,           \
            unsigned int: __builtin_u##op##_overflow,    \
            unsigned long: __builtin_u##op##l_overflow,  \
            unsigned long long: __builtin_u##op##ll_overflow)(x, y, out)
