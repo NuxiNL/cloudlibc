6f4428c84732fa1e1aa5d634749ebca54b4ce69b
Ed Schouten <ed@nuxi.nl>
2015-08-26 11:57:50 +0200

    Make <syslog.h> more complete, to ease porting.

---
+++ src/libc/syslog/openlog.c
@@ -0,0 +1,10 @@
+// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+//
+// This file is distrbuted under a 2-clause BSD license.
+// See the LICENSE file for details.
+
+#include <syslog.h>
+
+void openlog(const char *ident, int logopt, int facility) {
+  // Nothing to do.
+}
