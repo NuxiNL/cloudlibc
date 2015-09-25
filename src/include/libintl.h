1f95404859e8c1acd1cea28d98362441a5199e98
Ed Schouten <ed@nuxi.nl>
2015-09-25 23:12:34 +0200

    Use lowercase to match other headers.

--- src/include/libintl.h
+++ src/include/libintl.h
@@ -21,7 +21,7 @@
 // OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 // SUCH DAMAGE.
 
-// <libintl.h> - Iñtërnâtiônàlizætiøn
+// <libintl.h> - iñtërnâtiônàlizætiøn
 //
 // Features missing:
 // - dcgettext() and dcngettext():
