c912d03454462675c31f2f547713135464b18dad
Ed Schouten <ed@nuxi.nl>
2015-09-29 15:27:20 +0200

    Do include <_/types.h>.
    It turns out we need it for _NULL.

--- src/include/libintl.h
+++ src/include/libintl.h
@@ -34,7 +34,7 @@
 #ifndef _LIBINTL_H_
 #define _LIBINTL_H_
 
-#include <_/cdefs.h>
+#include <_/types.h>
 
 // As there is no process wide locale in this environment, the standard
 // Gettext interface should always return the input strings literally.
