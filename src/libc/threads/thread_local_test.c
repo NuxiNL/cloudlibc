d11ed612ae5258d93e9cb97b41969a6a21cda921
Ed Schouten <ed@nuxi.nl>
2015-10-14 16:59:17 +0200

    Add a test to validate TLS offset.

---
+++ src/libc/threads/thread_local_test.c
@@ -0,0 +1,33 @@
+// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+//
+// This file is distrbuted under a 2-clause BSD license.
+// See the LICENSE file for details.
+
+#include <testing.h>
+#include <threads.h>
+
+extern thread_local int tlsvar_i;
+thread_local int tlsvar_i = 0x4d653c57;
+extern thread_local int tlsvar_j;
+thread_local int tlsvar_j = 0;
+
+static int validate(void *arg) {
+  // Validate that thread local variables have proper initial values.
+  ASSERT_EQ(0x4d653c57, tlsvar_i);
+  ASSERT_EQ(0, tlsvar_j);
+  return 0;
+}
+
+TEST(thread_local, direct) {
+  validate(NULL);
+}
+
+TEST_SEPARATE_PROCESS(thread_local, process) {
+  validate(NULL);
+}
+
+TEST(thread_local, thread) {
+  thrd_t thread;
+  ASSERT_EQ(thrd_success, thrd_create(&thread, validate, NULL));
+  ASSERT_EQ(thrd_success, thrd_join(thread, NULL));
+}
