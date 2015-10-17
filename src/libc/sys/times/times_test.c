d5facdde4ea11b6c5e1fb5cae3491e81727d2066
Ed Schouten <ed@nuxi.nl>
2015-10-17 19:50:16 +0200

    Make <sys/times.h> complete.

---
+++ src/libc/sys/times/times_test.c
@@ -0,0 +1,33 @@
+// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+//
+// This file is distrbuted under a 2-clause BSD license.
+// See the LICENSE file for details.
+
+#include <sys/times.h>
+
+#include <testing.h>
+#include <time.h>
+#include <unistd.h>
+
+TEST(times, example) {
+  // Obtain running times of the process. As we don't support a process
+  // hierarchy through traditional forking, times for child processes
+  // should be zero.
+  struct tms tms_before;
+  clock_t before = times(&tms_before);
+  ASSERT_EQ(0, tms_before.tms_cutime);
+  ASSERT_EQ(0, tms_before.tms_cstime);
+
+  // Sleep one second.
+  ASSERT_EQ(0, sleep(1));
+
+  // Somewhere between 0.5 and 1.5 seconds should have passed.
+  struct tms tms_after;
+  clock_t after = times(&tms_after);
+  ASSERT_LE(CLOCKS_PER_SEC / 2, after - before);
+  ASSERT_GE(3 * CLOCKS_PER_SEC / 2, after - before);
+  ASSERT_EQ(0, tms_before.tms_cutime);
+  ASSERT_EQ(0, tms_before.tms_cstime);
+  ASSERT_LE(tms_before.tms_utime, tms_after.tms_utime);
+  ASSERT_LE(tms_before.tms_stime, tms_after.tms_stime);
+}
