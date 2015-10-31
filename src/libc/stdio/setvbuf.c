b82179bc955b34da4a350a7f5c39552957609e06
Ed Schouten <ed@nuxi.nl>
2015-10-31 09:27:51 +0100

    Validate setvbuf()'s buffer type argument.

--- src/libc/stdio/setvbuf.c
+++ src/libc/stdio/setvbuf.c
@@ -8,6 +8,10 @@
 #include <stdio.h>
 
 int setvbuf(FILE *restrict stream, char *restrict buf, int type, size_t size) {
+  if (type != _IOFBF && type != _IOLBF && type != _IONBF) {
+    errno = EINVAL;
+    return -1;
+  }
   flockfile(stream);
   bool result = fop_setvbuf(stream, size);
   if (result)
