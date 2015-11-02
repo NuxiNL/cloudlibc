94dbb7790c6a6c8f9ab9ebdee16bfc40513024f6
Ed Schouten <ed@nuxi.nl>
2015-11-02 08:39:44 +0100

    Add tests for flockfile() and ftrylockfile().

---
+++ src/libc/stdio/flockfile_test.c
@@ -0,0 +1,22 @@
+// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+//
+// This file is distrbuted under a 2-clause BSD license.
+// See the LICENSE file for details.
+
+#include <stdio.h>
+#include <testing.h>
+
+TEST(flockfile, example) __no_lock_analysis {
+  FILE *fp = tmpfile();
+  ASSERT_NE(NULL, fp);
+
+  // Acquire and release the lock recursively.
+  flockfile(fp);
+  flockfile(fp);
+  flockfile(fp);
+  funlockfile(fp);
+  funlockfile(fp);
+  funlockfile(fp);
+
+  ASSERT_EQ(0, fclose(fp));
+}
