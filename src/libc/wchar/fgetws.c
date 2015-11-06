63fd25633697c4783b77d5b4d7846727a62ceed5
Ed Schouten <ed@nuxi.nl>
2015-11-06 15:54:56 +0100

    Add an important note that a newline is always encoded as '\n'.

--- src/libc/wchar/fgetws.c
+++ src/libc/wchar/fgetws.c
@@ -50,6 +50,9 @@ wchar_t *fgetws(wchar_t *restrict ws, int n, FILE *restrict stream) {
     // read buffer for short reads, first limit the read buffer to a
     // safe upperbound. Assume that wide character text has up to two
     // bytes per character on average.
+    //
+    // Assume that for the character encoding a newline is always
+    // encoded as '\n'.
     size_t upperbound = (MB_LEN_MAX > outbuflen ? MB_LEN_MAX : outbuflen) * 2;
     if (readbuflen > upperbound)
       readbuflen = upperbound;
