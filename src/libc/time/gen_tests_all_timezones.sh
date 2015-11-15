61e7d58c4333d70bb34fa23e7f8dacc38d06ff8f
Ed Schouten <ed@nuxi.nl>
2015-11-15 09:47:59 +0100

    Use the latest implementation of zdump.
    It looks like the version shipped with my Ubuntu workstation has a bug
    that it miscomputes some of the Moroccan timezones.

--- src/libc/time/gen_tests_all_timezones.sh
+++ src/libc/time/gen_tests_all_timezones.sh
@@ -1,18 +1,28 @@
 #!/bin/sh
 set -e
+
 rm -Rf tmp
 mkdir tmp
 cd tmp
+
 tar -xf ../tzdata2015g.tar.gz
+tar -xf ../tzcode2015g.tar.gz
+
+make zdump > /dev/null
+
 mkdir output
 zic -d output africa antarctica asia australasia backward etcetera europe \
     northamerica pacificnew southamerica
+
 cd output
 i=0
 curdir=`pwd`
-find * -type f | while read f; do
+find * -type f | sort | while read f; do
   echo "TEST_BEGIN(generated_$i, \"$f\")"
-  zdump -v "$curdir/$f" | ../../convert_zdump_to_tests.py
+  ../zdump -v "$curdir/$f" | ../../convert_zdump_to_tests.py
   echo TEST_END
   i=$(($i+1))
 done
+
+cd ../..
+rm -Rf tmp
