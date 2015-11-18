7b03432abf20cac373169332110edfe485aa3183
Ed Schouten <ed@nuxi.nl>
2015-11-18 22:52:57 +0100

    Fix an embarrassing typo.

--- src/common/overflow.h
+++ src/common/overflow.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2015 Nuxi, https://nuxi.nl/
 //
-// This file is distrbuted under a 2-clause BSD license.
+// This file is distributed under a 2-clause BSD license.
 // See the LICENSE file for details.
 
 #ifndef COMMON_OVERFLOW_H
--- src/libc/netdb/gen_servent.py
+++ src/libc/netdb/gen_servent.py
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2015 Nuxi, https://nuxi.nl/
 #
-# This file is distrbuted under a 2-clause BSD license.
+# This file is distributed under a 2-clause BSD license.
 # See the LICENSE file for details.
 
 import collections
