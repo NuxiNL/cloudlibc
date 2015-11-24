67dcd9aa7389cdd310bcbb25b3d7f56e935cffa8
Ed Schouten <ed@nuxi.nl>
2015-11-24 10:06:04 +0100

    Add a test for the blocking behaviour of mq_timedsend().

--- src/libc/mqueue/mq_timedsend_test.c
+++ src/libc/mqueue/mq_timedsend_test.c
@@ -26,7 +26,7 @@ TEST(mq_timedsend, bad) {
   ASSERT_EQ(
       0, mq_timedsend(mqd, "Hello", 5, 123, &(struct timespec){.tv_nsec = -1}));
 
-  // The queue should now be full, meaning we ahve to return EAGAIN.
+  // The queue should now be full, meaning we have to return EAGAIN.
   ASSERT_EQ(-1, mq_timedsend(mqd, "Hello", 5, 123, &(struct timespec){}));
   ASSERT_EQ(EAGAIN, errno);
 
@@ -47,4 +47,46 @@ TEST(mq_timedsend, bad) {
   ASSERT_EQ(0, mq_destroy(mqd));
 }
 
-// TODO(ed): Add more tests.
+static void *pull_message(void *argument) {
+  // Pull the first message from the message queue after a slight delay.
+  ASSERT_EQ(0, clock_nanosleep(CLOCK_MONOTONIC, 0,
+                               &(struct timespec){.tv_nsec = 250000000}));
+  char buf[5];
+  unsigned int prio;
+  ASSERT_EQ(5, mq_receive(*(mqd_t *)argument, buf, sizeof(buf), &prio));
+  ASSERT_ARREQ("Hello", buf, sizeof(buf));
+  ASSERT_EQ(123, prio);
+  return NULL;
+}
+
+TEST(mq_timedsend, blocking) {
+  // Initialize a queue that is already full. As we won't block, it
+  // should be allowed to provide an invalid abstime.
+  mqd_t mqd;
+  struct mq_attr attr = {
+      .mq_flags = 0, .mq_maxmsg = 1, .mq_msgsize = 5,
+  };
+  ASSERT_EQ(0, mq_init(&mqd, &attr));
+  ASSERT_EQ(
+      0, mq_timedsend(mqd, "Hello", 5, 123, &(struct timespec){.tv_nsec = -1}));
+
+  // Spawn a background thread to empty the queue with a little delay.
+  pthread_t thread;
+  ASSERT_EQ(0, pthread_create(&thread, NULL, pull_message, &mqd));
+
+  // Block on the message queue while sending a message.
+  struct timespec abstime;
+  ASSERT_EQ(0, clock_gettime(CLOCK_REALTIME, &abstime));
+  ++abstime.tv_sec;
+  ASSERT_EQ(0, mq_timedsend(mqd, "World", 5, 456, &abstime));
+
+  // The second message should be immediately readable.
+  char buf[5];
+  unsigned int prio;
+  ASSERT_EQ(5, mq_receive(mqd, buf, sizeof(buf), &prio));
+  ASSERT_ARREQ("World", buf, sizeof(buf));
+  ASSERT_EQ(456, prio);
+
+  ASSERT_EQ(0, pthread_join(thread, NULL));
+  ASSERT_EQ(0, mq_destroy(mqd));
+}
