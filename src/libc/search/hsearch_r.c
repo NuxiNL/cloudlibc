218ff3537f6e4f04438e57b32cd7f7ab77367303
Ed Schouten <ed@nuxi.nl>
2015-12-15 09:21:40 +0100

    Remove the cached hash from the hash table entry structures.
    Just recompute the hashes when growing the hash table. I've done some
    measurements and it seems that the performance gain of caching the
    hashes is very minimal. Removing the hashes reduces the table size by
    one third.

--- src/libc/search/hsearch_r.c
+++ src/libc/search/hsearch_r.c
@@ -13,39 +13,43 @@
 // Look up an unused entry in the hash table for a given hash. For this
 // implementation we use quadratic probing. This is done to prevent the
 // need for recomputing the keys when growing the hash table.
-static inline struct hentry *hsearch_lookup_free(struct __hsearch *hsearch,
-                                                 size_t hash) {
+static ENTRY *hsearch_lookup_free(struct __hsearch *hsearch, size_t hash) {
   for (size_t index = hash, i = 0;; index += ++i) {
-    struct hentry *hentry = &hsearch->entries[index & hsearch->index_mask];
-    if (hentry->entry.key == NULL)
-      return hentry;
+    ENTRY *entry = &hsearch->entries[index & hsearch->index_mask];
+    if (entry->key == NULL)
+      return entry;
   }
 }
 
-int hsearch_r(ENTRY item, ACTION action, ENTRY **retval,
-              struct hsearch_data *htab) {
-  // Compute an FNV-1a hash of the key. Depending on the pointer size,
-  // this either uses the 32- or 64-bit FNV prime.
-  struct __hsearch *hsearch = htab->__hsearch;
-  size_t hash = hsearch->offset_basis;
-  for (const char *str = item.key; *str != '\0'; ++str) {
-    hash ^= (uint8_t)*str;
+// Computes an FNV-1a hash of the key. Depending on the pointer size, this
+// either uses the 32- or 64-bit FNV prime.
+static size_t hsearch_hash(size_t offset_basis, const char *str) {
+  size_t hash = offset_basis;
+  while (*str != '\0') {
+    hash ^= (uint8_t)*str++;
     // clang-format off
     hash *= _Generic((size_t)0,
                      uint32_t: UINT32_C(16777619),
                      uint64_t: UINT64_C(1099511628211));
     // clang-format on
   }
+  return hash;
+}
+
+int hsearch_r(ENTRY item, ACTION action, ENTRY **retval,
+              struct hsearch_data *htab) {
+  struct __hsearch *hsearch = htab->__hsearch;
+  size_t hash = hsearch_hash(hsearch->offset_basis, item.key);
 
   // Search the hash table for an existing entry for this key. Stop
   // searching if we run into an unused hash table entry.
-  struct hentry *hentry;
+  ENTRY *entry;
   for (size_t index = hash, i = 0;; index += ++i) {
-    hentry = &hsearch->entries[index & hsearch->index_mask];
-    if (hentry->entry.key == NULL)
+    entry = &hsearch->entries[index & hsearch->index_mask];
+    if (entry->key == NULL)
       break;
-    if (strcmp(hentry->entry.key, item.key) == 0) {
-      *retval = &hentry->entry;
+    if (strcmp(entry->key, item.key) == 0) {
+      *retval = entry;
       return 1;
     }
   }
@@ -68,9 +72,11 @@ int hsearch_r(ENTRY item, ACTION action, ENTRY **retval,
     new_hsearch->offset_basis = hsearch->offset_basis;
     new_hsearch->entries_used = hsearch->entries_used;
     for (size_t i = 0; i <= hsearch->index_mask; ++i) {
-      const struct hentry *old_hentry = &hsearch->entries[i];
-      if (old_hentry->entry.key != NULL)
-        *hsearch_lookup_free(new_hsearch, old_hentry->hash) = *old_hentry;
+      const ENTRY *old_entry = &hsearch->entries[i];
+      if (old_entry->key != NULL) {
+        size_t old_hash = hsearch_hash(hsearch->offset_basis, old_entry->key);
+        *hsearch_lookup_free(new_hsearch, old_hash) = *old_entry;
+      }
     }
 
     // Replace and destroy the old hash table.
@@ -80,13 +86,12 @@ int hsearch_r(ENTRY item, ACTION action, ENTRY **retval,
     // Perform a new lookup for a free table entry, so that we insert
     // the entry into the new hash table.
     hsearch = htab->__hsearch;
-    hentry = hsearch_lookup_free(hsearch, hash);
+    entry = hsearch_lookup_free(hsearch, hash);
   }
 
   // Insert the new entry into the hash table.
-  hentry->hash = hash;
-  hentry->entry = item;
+  *entry = item;
   ++hsearch->entries_used;
-  *retval = &hentry->entry;
+  *retval = entry;
   return 1;
 }
