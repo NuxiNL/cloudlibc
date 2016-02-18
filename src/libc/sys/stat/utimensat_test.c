e41f51505ed15d3bdf8eb33ff886c15dbff6f4d0
Ed Schouten <ed@nuxi.nl>
2016-02-18 21:08:14 +0100

    Make the utimensat() test a bit less strict.
    It turns out that on Linux, symbolic links also have their atimes bumped
    when traversed. Reduce the test to only testing the behaviour of
    AT_SYMLINK_NOFOLLOW once, as this is sufficient. In all other cases,
    just use the FIFO's pathname instead of that of the symbolic link.

--- src/libc/sys/stat/utimensat_test.c
+++ src/libc/sys/stat/utimensat_test.c
@@ -18,14 +18,14 @@ TEST(utimensat, example) {
 
   // Set timestamps to known initial values.
   {
-    struct timespec times[2] = {{.tv_sec = 123, .tv_nsec = 456},
-                                {.tv_sec = 234, .tv_nsec = 567}};
-    ASSERT_EQ(0, utimensat(fd_tmp, "symlink", times, AT_SYMLINK_NOFOLLOW));
-  }
-  {
     struct timespec times[2] = {{.tv_sec = 345, .tv_nsec = 678},
                                 {.tv_sec = 901, .tv_nsec = 234}};
     ASSERT_EQ(0, utimensat(fd_tmp, "symlink", times, 0));
+  }
+  {
+    struct timespec times[2] = {{.tv_sec = 123, .tv_nsec = 456},
+                                {.tv_sec = 234, .tv_nsec = 567}};
+    ASSERT_EQ(0, utimensat(fd_tmp, "symlink", times, AT_SYMLINK_NOFOLLOW));
   }
 
   // Validate filetypes and timestamps before continuing.
@@ -37,7 +37,7 @@ TEST(utimensat, example) {
   ASSERT_EQ(234, sb.st_mtim.tv_sec);
   ASSERT_EQ(567, sb.st_mtim.tv_nsec);
 
-  ASSERT_EQ(0, fstatat(fd_tmp, "symlink", &sb, 0));
+  ASSERT_EQ(0, fstatat(fd_tmp, "fifo", &sb, 0));
   ASSERT_TRUE(S_ISFIFO(sb.st_mode));
   ASSERT_EQ(345, sb.st_atim.tv_sec);
   ASSERT_EQ(678, sb.st_atim.tv_nsec);
@@ -53,7 +53,7 @@ TEST(utimensat, example) {
   {
     struct timespec times[2] = {{.tv_nsec = UTIME_OMIT},
                                 {.tv_sec = 555, .tv_nsec = 666}};
-    ASSERT_EQ(0, utimensat(fd_tmp, "symlink", times, 0));
+    ASSERT_EQ(0, utimensat(fd_tmp, "fifo", times, 0));
   }
 
   ASSERT_EQ(0, fstatat(fd_tmp, "symlink", &sb, AT_SYMLINK_NOFOLLOW));
@@ -62,7 +62,7 @@ TEST(utimensat, example) {
   ASSERT_EQ(234, sb.st_mtim.tv_sec);
   ASSERT_EQ(567, sb.st_mtim.tv_nsec);
 
-  ASSERT_EQ(0, fstatat(fd_tmp, "symlink", &sb, 0));
+  ASSERT_EQ(0, fstatat(fd_tmp, "fifo", &sb, 0));
   ASSERT_EQ(345, sb.st_atim.tv_sec);
   ASSERT_EQ(678, sb.st_atim.tv_nsec);
   ASSERT_EQ(555, sb.st_mtim.tv_sec);
@@ -77,7 +77,7 @@ TEST(utimensat, example) {
   {
     struct timespec times[2] = {{.tv_nsec = UTIME_NOW},
                                 {.tv_nsec = UTIME_OMIT}};
-    ASSERT_EQ(0, utimensat(fd_tmp, "symlink", times, 0));
+    ASSERT_EQ(0, utimensat(fd_tmp, "fifo", times, 0));
   }
 
   ASSERT_EQ(0, fstatat(fd_tmp, "symlink", &sb, AT_SYMLINK_NOFOLLOW));
@@ -85,20 +85,20 @@ TEST(utimensat, example) {
   ASSERT_EQ(888, sb.st_atim.tv_nsec);
   ASSERT_LE(TIME_PASSED, sb.st_mtime);
 
-  ASSERT_EQ(0, fstatat(fd_tmp, "symlink", &sb, 0));
+  ASSERT_EQ(0, fstatat(fd_tmp, "fifo", &sb, 0));
   ASSERT_LE(TIME_PASSED, sb.st_atime);
   ASSERT_EQ(555, sb.st_mtim.tv_sec);
   ASSERT_EQ(666, sb.st_mtim.tv_nsec);
 
   // Null pointer.
   ASSERT_EQ(0, utimensat(fd_tmp, "symlink", NULL, AT_SYMLINK_NOFOLLOW));
-  ASSERT_EQ(0, utimensat(fd_tmp, "symlink", NULL, 0));
+  ASSERT_EQ(0, utimensat(fd_tmp, "fifo", NULL, 0));
 
   ASSERT_EQ(0, fstatat(fd_tmp, "symlink", &sb, AT_SYMLINK_NOFOLLOW));
   ASSERT_LE(TIME_PASSED, sb.st_atime);
   ASSERT_LE(TIME_PASSED, sb.st_mtime);
 
-  ASSERT_EQ(0, fstatat(fd_tmp, "symlink", &sb, 0));
+  ASSERT_EQ(0, fstatat(fd_tmp, "fifo", &sb, 0));
   ASSERT_LE(TIME_PASSED, sb.st_atime);
   ASSERT_LE(TIME_PASSED, sb.st_mtime);
 }
