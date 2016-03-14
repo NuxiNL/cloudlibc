e33aad8b40bd55c5b2931c2e0db3385e5fe3d53e
Ed Schouten <ed@nuxi.nl>
2016-03-14 08:34:15 +0100

    Remove support for LLVM <3.8.
    Now that CloudABI Ports has successfully switched over to Clang 3.8,
    let's go ahead and remove the hacks that were put in place to keep
    cloudlibc working with Clang 3.7.
    
    - Remove -Wno-null-dereference. This flag was put in place to suppress
      spurious compiler warnings generated by some prerelease versions of
      Clang 3.8..
    
    - Remove fallback code that is used in the absence of
      __builtin_{add,mul,sub}_overflow(). Clang supports these intrinsics
      unconditionally.

--- src/common/overflow.h
+++ src/common/overflow.h
@@ -6,63 +6,11 @@
 #ifndef COMMON_OVERFLOW_H
 #define COMMON_OVERFLOW_H
 
-#include <_/cdefs.h>
-
 // Performs an addition, subtraction or multiplication operation,
-// returning whether the computation caused an overflow.
-//
-// These intrinsics are only available as of Clang 3.8 and GCC 5.
-// For previous versions of Clang we must use builtins that are not type
-// generic.
-//
-// TODO(ed): Simplify this code once we use Clang 3.8 everywhere.
-
-#if __has_builtin(__builtin_add_overflow)
-
+// returning whether the computation caused an overflow. These
+// intrinsics are available as of Clang 3.8 and GCC 5.
 #define add_overflow(x, y, out) __builtin_add_overflow(x, y, out)
 #define sub_overflow(x, y, out) __builtin_sub_overflow(x, y, out)
 #define mul_overflow(x, y, out) __builtin_mul_overflow(x, y, out)
-
-#else
-
-#include <limits.h>
-#include <stdbool.h>
-
-// Implementations for types for which no intrinsics exist.
-static inline bool uaddh_overflow(unsigned short a, unsigned short b,
-                                  unsigned short *c) {
-  *c = a + b;
-  return a > USHRT_MAX - b;
-}
-
-static inline bool usubh_overflow(unsigned short a, unsigned short b,
-                                  unsigned short *c) {
-  *c = a - b;
-  return a < b;
-}
-
-static inline bool umulh_overflow(unsigned short a, unsigned short b,
-                                  unsigned short *c) {
-  *c = a * b;
-  return b != 0 && a > USHRT_MAX / b;
-}
-
-// clang-format off
-#define COMPUTE_OVERFLOW(op, x, y, out)                 \
-  _Generic(*(out),                                      \
-           int: __builtin_s##op##_overflow,             \
-           long: __builtin_s##op##l_overflow,           \
-           long long: __builtin_s##op##ll_overflow,     \
-           unsigned short: u##op##h_overflow,           \
-           unsigned int: __builtin_u##op##_overflow,    \
-           unsigned long: __builtin_u##op##l_overflow,  \
-           unsigned long long: __builtin_u##op##ll_overflow)(x, y, out)
-// clang-format on
-
-#define add_overflow(x, y, out) COMPUTE_OVERFLOW(add, x, y, out)
-#define sub_overflow(x, y, out) COMPUTE_OVERFLOW(sub, x, y, out)
-#define mul_overflow(x, y, out) COMPUTE_OVERFLOW(mul, x, y, out)
-
-#endif
 
 #endif
