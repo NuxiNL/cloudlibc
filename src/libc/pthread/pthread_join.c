fe522268264c1239c67e0439153e46e150a626b3
Ed Schouten <ed@nuxi.nl>
2016-03-27 20:27:44 +0200

    Extract a reference to the unsafe stack before freeing the safe stack.
    The safe stack is responsible for holding the thread handle as well. The
    first call to free() may therefore invalidate the pointer to the unsafe
    stack.

--- src/libc/pthread/pthread_join.c
+++ src/libc/pthread/pthread_join.c
@@ -47,7 +47,8 @@ int pthread_join(pthread_t thread, void **value_ptr) {
 
   // Free the stack buffers associated with this thread. The safe stack
   // buffer also contains the thread's handle.
+  void *unsafe_stack = thread->unsafe_stack;
   free(thread->safe_stack);
-  free(thread->unsafe_stack);
+  free(unsafe_stack);
   return 0;
 }
