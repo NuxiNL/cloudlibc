9ea2adf40d9cce28bbf1e12114759f6d0b72f540
Ed Schouten <ed@nuxi.nl>
2016-03-27 20:44:21 +0200

    Allow the link count to be less than 2.
    It turns out that btrfs doesn't follow the tradition of letting the link
    count on directories be equal to 2 + nfiles. Be a bit more lenient.

--- src/libc/sys/stat/mkdirat_test.c
+++ src/libc/sys/stat/mkdirat_test.c
@@ -13,5 +13,8 @@ TEST(mkdirat, simple) {
   struct stat sb;
   ASSERT_EQ(0, fstatat(fd_tmp, "example", &sb, 0));
   ASSERT_TRUE(S_ISDIR(sb.st_mode));
-  ASSERT_EQ(2, sb.st_nlink);
+  // Though most UNIX file systems use a link count of 2 for empty
+  // directories, btrfs does not. It always uses a link count of 1.
+  ASSERT_LE(1, sb.st_nlink);
+  ASSERT_GE(2, sb.st_nlink);
 }
