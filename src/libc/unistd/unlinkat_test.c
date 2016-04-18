c5d553a76d9db44d7c72b5eeb4a7bc6b487dbccb
Ed Schouten <ed@nuxi.nl>
2016-04-18 10:11:43 +0200

    Add some tests for dealing with trailing slashes on pathnames.
    Trailing slashes should cause us to follow symlinks on the target. This
    should only be allowed in case the target of the symlink is a directory.
    Mac OS X seems to do some flaky things there, but we work around that in
    the emulator.

--- src/libc/unistd/unlinkat_test.c
+++ src/libc/unistd/unlinkat_test.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+// Copyright (c) 2015-2016 Nuxi, https://nuxi.nl/
 //
 // This file is distributed under a 2-clause BSD license.
 // See the LICENSE file for details.
@@ -29,4 +29,32 @@ TEST(unlinkat, examples) {
   ASSERT_EQ(0, unlinkat(fd_tmp, "dir", AT_REMOVEDIR));
   ASSERT_EQ(-1, unlinkat(fd_tmp, "dir", 0));
   ASSERT_EQ(ENOENT, errno);
+}
+
+TEST(unlinkat, symlinks_directory) {
+  // A symlink to a directory is not a directory.
+  ASSERT_EQ(0, mkdirat(fd_tmp, "dir"));
+  ASSERT_EQ(0, symlinkat("dir", fd_tmp, "symlink"));
+  ASSERT_EQ(-1, unlinkat(fd_tmp, "symlink", AT_REMOVEDIR));
+  ASSERT_EQ(ENOTDIR, errno);
+
+  // Trailing slash should cause us to follow the directory and remove
+  // that instead.
+  ASSERT_EQ(-1, unlinkat(fd_tmp, "symlink/", 0));
+  ASSERT_EQ(EPERM, errno);
+  ASSERT_EQ(0, unlinkat(fd_tmp, "symlink/", AT_REMOVEDIR));
+  ASSERT_EQ(-1, unlinkat(fd_tmp, "dir", 0));
+  ASSERT_EQ(ENOENT, errno);
+}
+
+TEST(unlinkat, symlinks_fifo) {
+  // The trailing slash should only be usable on symlinks to
+  // directories. On symlinks to non-directories, we should always
+  // return ENOTDIR.
+  ASSERT_EQ(0, mkfifoat(fd_tmp, "fifo"));
+  ASSERT_EQ(0, symlinkat("fifo", fd_tmp, "symlink"));
+  ASSERT_EQ(-1, unlinkat(fd_tmp, "symlink/", AT_REMOVEDIR));
+  ASSERT_EQ(ENOTDIR, errno);
+  ASSERT_EQ(-1, unlinkat(fd_tmp, "symlink/", 0));
+  ASSERT_EQ(ENOTDIR, errno);
 }
