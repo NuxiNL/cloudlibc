d3aff5f0cc3ca612f52896be9b0db291db8b5488
Ed Schouten <ed@nuxi.nl>
2016-05-23 20:28:12 +0200

    Make our implementation of a64l() 40% smaller.
    Get rid of most of the branching by using a table to convert an ASCII
    character to a numerical value. The ASCII characters are all pretty
    close to each other. The table only needs to have 77 entries, which is
    not that many more than 64.

--- src/libc/stdlib/a64l.c
+++ src/libc/stdlib/a64l.c
@@ -9,22 +9,27 @@
 long a64l(const char *s) {
   int32_t value = 0;
   for (size_t i = 0; i < 6; ++i) {
-    char c = s[i];
-    int32_t digit;
-    if (c == '.') {
-      digit = 0;
-    } else if (c == '/') {
-      digit = 1;
-    } else if (c >= '0' && c <= '9') {
-      digit = c - '0' + 2;
-    } else if (c >= 'A' && c <= 'Z') {
-      digit = c - 'A' + 12;
-    } else if (c >= 'a' && c <= 'z') {
-      digit = c - 'a' + 38;
-    } else {
+    static const char chars[] = {
+#define MAP(a, b) [(a) - '.'] = (b) + 1
+        MAP('.', 0),  MAP('/', 1),  MAP('0', 2),  MAP('1', 3),  MAP('2', 4),
+        MAP('3', 5),  MAP('4', 6),  MAP('5', 7),  MAP('6', 8),  MAP('7', 9),
+        MAP('8', 10), MAP('9', 11), MAP('A', 12), MAP('B', 13), MAP('C', 14),
+        MAP('D', 15), MAP('E', 16), MAP('F', 17), MAP('G', 18), MAP('H', 19),
+        MAP('I', 20), MAP('J', 21), MAP('K', 22), MAP('L', 23), MAP('M', 24),
+        MAP('N', 25), MAP('O', 26), MAP('P', 27), MAP('Q', 28), MAP('R', 29),
+        MAP('S', 30), MAP('T', 31), MAP('U', 32), MAP('V', 33), MAP('W', 34),
+        MAP('X', 35), MAP('Y', 36), MAP('Z', 37), MAP('a', 38), MAP('b', 39),
+        MAP('c', 40), MAP('d', 41), MAP('e', 42), MAP('f', 43), MAP('g', 44),
+        MAP('h', 45), MAP('i', 46), MAP('j', 47), MAP('k', 48), MAP('l', 49),
+        MAP('m', 50), MAP('n', 51), MAP('o', 52), MAP('p', 53), MAP('q', 54),
+        MAP('r', 55), MAP('s', 56), MAP('t', 57), MAP('u', 58), MAP('v', 59),
+        MAP('w', 60), MAP('x', 61), MAP('y', 62), MAP('z', 63),
+#undef MAP
+    };
+    unsigned char c = s[i] - '.';
+    if (c >= __arraycount(chars) || chars[c] == 0)
       break;
-    }
-    value |= digit << i * 6;
+    value |= (chars[c] - 1) << i * 6;
   }
   return value;
 }
