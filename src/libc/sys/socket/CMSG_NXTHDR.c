8f94285f5e108661c37e7fa670bceb5fd948cc4c
Ed Schouten <ed@nuxi.nl>
2016-06-03 08:30:09 +0200

    Put another safety belt in place.
    Bail out when calling CMSG_NXTHDR() on a control message header that has
    a malformed cmsg_len. This prevents potential infinite loops.

--- src/libc/sys/socket/CMSG_NXTHDR.c
+++ src/libc/sys/socket/CMSG_NXTHDR.c
@@ -5,12 +5,24 @@
 
 #include <sys/socket.h>
 
+#include <assert.h>
+#include <stddef.h>
 #include <stdint.h>
 
 #include "socket_impl.h"
 
 struct cmsghdr *CMSG_NXTHDR(const struct msghdr *mhdr,
                             const struct cmsghdr *cmsg) {
-  return CMSG_GET(mhdr, cmsg == NULL ? (uintptr_t)mhdr->msg_controllen
-                                     : (uintptr_t)cmsg + cmsg->cmsg_len);
+  // RFC 2292:if the value of the cmsg pointer is NULL, a pointer to
+  // the cmsghdr structure describing the first ancillary data object
+  // is returned.
+  if (cmsg == NULL)
+    return CMSG_GET(mhdr, (uintptr_t)mhdr->msg_controllen);
+
+  // Safety belt: CMSG_NXTHDR() should not be called on a malformed
+  // control message header. This could cause an infinite loop if
+  // cmsg_len were zero.
+  assert(cmsg->cmsg_len >= offsetof(struct cmsghdr, __cmsg_data) &&
+         "Message header has negative data length");
+  return CMSG_GET(mhdr, (uintptr_t)cmsg + cmsg->cmsg_len);
 }
