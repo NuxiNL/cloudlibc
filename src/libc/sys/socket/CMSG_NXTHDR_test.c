4d90f5c48f817004cec97f08a88e960404ab8270
Ed Schouten <ed@nuxi.nl>
2016-06-03 12:41:03 +0200

    Add tests for CMSG_NXTHDR().

---
+++ src/libc/sys/socket/CMSG_NXTHDR_test.c
@@ -0,0 +1,72 @@
+// Copyright (c) 2016 Nuxi, https://nuxi.nl/
+//
+// This file is distributed under a 2-clause BSD license.
+// See the LICENSE file for details.
+
+#include <sys/socket.h>
+
+#include <stdalign.h>
+#include <stddef.h>
+#include <testing.h>
+
+TEST(CMSG_NXTHDR, first) {
+  // No ancillary data.
+  {
+    struct msghdr mhdr = {};
+    ASSERT_EQ(NULL, CMSG_NXTHDR(&mhdr, NULL));
+  }
+
+  // Zero-sized ancillary data.
+  {
+    struct cmsghdr cmsg;
+    struct msghdr mhdr = {.msg_control = &cmsg};
+    ASSERT_EQ(NULL, CMSG_NXTHDR(&mhdr, NULL));
+  }
+
+  // Ancillary data is too small to fit any data.
+  {
+    struct cmsghdr cmsg;
+    struct msghdr mhdr = {
+        .msg_control = &cmsg, .msg_controllen = CMSG_LEN(0) - 1,
+    };
+    ASSERT_EQ(NULL, CMSG_NXTHDR(&mhdr, NULL));
+  }
+
+  // Ancillary data is just large enough.
+  {
+    struct cmsghdr cmsg;
+    struct msghdr mhdr = {
+        .msg_control = &cmsg, .msg_controllen = CMSG_LEN(0),
+    };
+    ASSERT_EQ(&cmsg, CMSG_NXTHDR(&mhdr, NULL));
+  }
+}
+
+TEST(CMSG_NXTHDR, second) {
+  for (size_t i = 0; i < 100; ++i) {
+    size_t firstlen = CMSG_SPACE(i);
+    alignas(struct cmsghdr) char cmsg[firstlen + CMSG_LEN(0)];
+
+    // Ancillary data is too small to fit a second element.
+    {
+      struct msghdr mhdr = {
+          .msg_control = cmsg, .msg_controllen = firstlen + CMSG_LEN(0) - 1,
+      };
+      struct cmsghdr *first = CMSG_NXTHDR(&mhdr, NULL);
+      first->cmsg_len = CMSG_LEN(i);
+      ASSERT_EQ(cmsg, first);
+      ASSERT_EQ(NULL, CMSG_NXTHDR(&mhdr, first));
+    }
+
+    // Ancillary data is just large enough to fit a second element.
+    {
+      struct msghdr mhdr = {
+          .msg_control = cmsg, .msg_controllen = firstlen + CMSG_LEN(0),
+      };
+      struct cmsghdr *first = CMSG_NXTHDR(&mhdr, NULL);
+      first->cmsg_len = CMSG_LEN(i);
+      ASSERT_EQ(cmsg, first);
+      ASSERT_EQ(cmsg + firstlen, CMSG_NXTHDR(&mhdr, first));
+    }
+  }
+}
