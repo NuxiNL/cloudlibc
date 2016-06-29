12c3e6cda15458e02b3d82b582ee06bf2c6864c1
Ed Schouten <ed@nuxi.nl>
2016-06-29 16:33:34 +0200

    Improve simplicity.
    There is no use for the intermediate variable.

--- src/libc/time/localtime_utc.c
+++ src/libc/time/localtime_utc.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+// Copyright (c) 2015-2016 Nuxi, https://nuxi.nl/
 //
 // This file is distributed under a 2-clause BSD license.
 // See the LICENSE file for details.
@@ -13,7 +13,7 @@
 
 #define ERALENGTH ((time_t)146097 * 86400)
 
-int __localtime_utc(time_t timer, struct tm *result) {
+int __localtime_utc(time_t timer, struct tm *tm) {
   // The calendar repeats every 146097 days (400 years). Normalize the
   // timer to lie within this range. This ensures that the timestamp is
   // positive, simplifying the arithmetic below.
@@ -26,22 +26,21 @@ int __localtime_utc(time_t timer, struct tm *result) {
 
   // Compute time within the day.
   uint_fast24_t seconds = (uint_fast40_t)timer % 86400;
-  struct tm tm = {};
-  tm.tm_sec = seconds % 60;
+  *tm = (struct tm){.tm_sec = seconds % 60};
   uint_fast16_t minutes = seconds / 60;
-  tm.tm_min = minutes % 60;
-  tm.tm_hour = minutes / 60;
+  tm->tm_min = minutes % 60;
+  tm->tm_hour = minutes / 60;
 
   // Compute weekday. Day 0 is a Thursday.
   uint_fast24_t days = (uint_fast40_t)timer / 86400;
-  tm.tm_wday = (days + 4) % 7;
+  tm->tm_wday = (days + 4) % 7;
 
   // Apply leap days, so that every year becomes exactly 366 days long.
   // First add the three leap days at the boundaries of the centuries.
   // After that we can add up to three leap days per 1461 days.
   days += ((int_fast24_t)days - 11323) / 36524;
   days += days / 1461 + (days + 731) / 1461 + (days + 1096) / 1461;
-  tm.tm_yday = days % 366;
+  tm->tm_yday = days % 366;
 
   // Compute the year.
   uint_fast16_t local_year = days / 366 + 70;
@@ -49,23 +48,20 @@ int __localtime_utc(time_t timer, struct tm *result) {
 
   // Compute month and day within month.
   const short *months = get_months(local_year);
-  while (tm.tm_yday >= months[tm.tm_mon + 1])
-    ++tm.tm_mon;
-  tm.tm_mday = tm.tm_yday - months[tm.tm_mon] + 1;
+  while (tm->tm_yday >= months[tm->tm_mon + 1])
+    ++tm->tm_mon;
+  tm->tm_mday = tm->tm_yday - months[tm->tm_mon] + 1;
 
   // Determine whether the result fits in "struct tm". If it does not,
   // still return a structure, but set the year to INT_MIN or INT_MAX.
-  int error;
   if (year < INT_MIN) {
-    tm.tm_year = INT_MIN;
-    error = EOVERFLOW;
+    tm->tm_year = INT_MIN;
+    return EOVERFLOW;
   } else if (year > INT_MAX) {
-    tm.tm_year = INT_MAX;
-    error = EOVERFLOW;
+    tm->tm_year = INT_MAX;
+    return EOVERFLOW;
   } else {
-    tm.tm_year = year;
-    error = 0;
+    tm->tm_year = year;
+    return 0;
   }
-  *result = tm;
-  return error;
 }
