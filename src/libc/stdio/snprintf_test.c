1de0064ad59dbed2537229b96bcb5a971614a608
Ed Schouten <ed@nuxi.nl>
2016-07-14 17:52:37 +0200

    Add a convenience macro for testing the output of snprintf().

--- src/libc/stdio/snprintf_test.c
+++ src/libc/stdio/snprintf_test.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+// Copyright (c) 2015-2016 Nuxi, https://nuxi.nl/
 //
 // This file is distributed under a 2-clause BSD license.
 // See the LICENSE file for details.
@@ -10,15 +10,20 @@
 #include <stdio.h>
 #include <testing.h>
 
+#define TEST_OUTPUT(out, ...)                                            \
+  do {                                                                   \
+    char buf[sizeof(out)];                                               \
+    ASSERT_EQ(sizeof(buf) - 1, snprintf(buf, sizeof(buf), __VA_ARGS__)); \
+    ASSERT_STREQ(out, buf);                                              \
+  } while (0);
+
 TEST(snprintf, null) {
   ASSERT_EQ(12, snprintf(NULL, 0, "Hello, world"));
 }
 
 TEST(snprintf, null_string) {
-  char buf[14];
   char *ptr = NULL;
-  ASSERT_EQ(13, snprintf(buf, sizeof(buf), "Hello%.3sworld", ptr));
-  ASSERT_STREQ("Hello(nuworld", buf);
+  TEST_OUTPUT("Hello(nuworld", "Hello%.3sworld", ptr);
 }
 
 TEST(snprintf, truncation) {
@@ -28,15 +33,11 @@ TEST(snprintf, truncation) {
 }
 
 TEST(snprintf, number_width_precision_left) {
-  char buf[30];
-  ASSERT_EQ(20, snprintf(buf, sizeof(buf), "%20.10d", 5));
-  ASSERT_STREQ("          0000000005", buf);
+  TEST_OUTPUT("          0000000005", "%20.10d", 5);
 }
 
 TEST(snprintf, number_width_precision_right) {
-  char buf[30];
-  ASSERT_EQ(20, snprintf(buf, sizeof(buf), "%-20.10d", 5));
-  ASSERT_STREQ("0000000005          ", buf);
+  TEST_OUTPUT("0000000005          ", "%-20.10d", 5);
 }
 
 TEST(snprintf, grouping_en_us) {
@@ -58,189 +59,159 @@ TEST(snprintf, grouping_ru_ru) {
 }
 
 TEST(snprintf, numbered_arguments) {
-  char buf[4];
-  ASSERT_EQ(3, snprintf(buf, sizeof(buf), "%2$d %1$d", 5, 7));
-  ASSERT_STREQ("7 5", buf);
+  TEST_OUTPUT("7 5", "%2$d %1$d", 5, 7);
 }
 
 TEST(snprintf, hex_left) {
-  char buf[21];
-  ASSERT_EQ(20, snprintf(buf, sizeof(buf), "%-#20.10x", 0x1337));
-  ASSERT_STREQ("0x0000001337        ", buf);
+  TEST_OUTPUT("0x0000001337        ", "%-#20.10x", 0x1337);
 }
 
 TEST(snprintf, hex_right) {
-  char buf[21];
-  ASSERT_EQ(20, snprintf(buf, sizeof(buf), "%#20.10x", 0x1337));
-  ASSERT_STREQ("        0x0000001337", buf);
+  TEST_OUTPUT("        0x0000001337", "%#20.10x", 0x1337);
 }
 
 TEST(snprintf, octal1) {
-  char buf[2];
-  ASSERT_EQ(1, snprintf(buf, sizeof(buf), "%o", 1));
-  ASSERT_STREQ("1", buf);
+  TEST_OUTPUT("1", "%o", 1);
 }
 
 TEST(snprintf, octal2) {
-  char buf[3];
-  ASSERT_EQ(2, snprintf(buf, sizeof(buf), "%#o", 1));
-  ASSERT_STREQ("01", buf);
+  TEST_OUTPUT("01", "%#o", 1);
 }
 
+#if 0  // TODO(ed): Enable!
+TEST(snprintf, float10_f_g) {
+  // No modifiers.
+  TEST_OUTPUT("|1.000000|", "|%f|", 1.0);
+  TEST_OUTPUT("|1.000000|", "|%#f|", 1.0);
+  TEST_OUTPUT("|1|", "|%g|", 1.0);
+  TEST_OUTPUT("|1.00000|", "|%#g|", 1.0);
+
+  // Field width.
+  TEST_OUTPUT("|  1.000000|", "|%10f|", 1.0);
+  TEST_OUTPUT("|  1.000000|", "|%#10f|", 1.0);
+  TEST_OUTPUT("|         1|", "|%10g|", 1.0);
+  TEST_OUTPUT("|   1.00000|", "|%#10g|", 1.0);
+
+  // Left justified.
+  TEST_OUTPUT("|1.000000  |", "|%-10f|", 1.0);
+  TEST_OUTPUT("|1.000000  |", "|%#-10f|", 1.0);
+  TEST_OUTPUT("|1         |", "|%-10g|", 1.0);
+  TEST_OUTPUT("|1.00000   |", "|%#-10g|", 1.0);
+
+  // No decimals.
+  TEST_OUTPUT("|         1|", "|%10.0f|", 1.0);
+  TEST_OUTPUT("|        1.|", "|%#10.0f|", 1.0);
+  TEST_OUTPUT("|         1|", "|%10.0g|", 1.0);
+  TEST_OUTPUT("|        1.|", "|%#10.0g|", 1.0);
+
+  // No decimals, left justified.
+  TEST_OUTPUT("|1         |", "|%-10.0f|", 1.0);
+  TEST_OUTPUT("|1.        |", "|%#-10.0f|", 1.0);
+  TEST_OUTPUT("|1         |", "|%-10.0g|", 1.0);
+  TEST_OUTPUT("|1.        |", "|%#-10.0g|", 1.0);
+}
+#endif
+
 TEST(snprintf, float10_e_zero) {
-  char buf[13];
-  ASSERT_EQ(12, snprintf(buf, sizeof(buf), "%e", 0.0));
-  ASSERT_STREQ("0.000000e+00", buf);
-  ASSERT_EQ(5, snprintf(buf, sizeof(buf), "%.0e", 0.0));
-  ASSERT_STREQ("0e+00", buf);
-  ASSERT_EQ(6, snprintf(buf, sizeof(buf), "%#.0e", 0.0));
-  ASSERT_STREQ("0.e+00", buf);
-  ASSERT_EQ(6, snprintf(buf, sizeof(buf), "% .0e", 0.0));
-  ASSERT_STREQ(" 0e+00", buf);
-  ASSERT_EQ(6, snprintf(buf, sizeof(buf), "%+.0e", 0.0));
-  ASSERT_STREQ("+0e+00", buf);
+  TEST_OUTPUT("0.000000e+00", "%e", 0.0);
+  TEST_OUTPUT("0e+00", "%.0e", 0.0);
+  TEST_OUTPUT("0.e+00", "%#.0e", 0.0);
+  TEST_OUTPUT(" 0e+00", "% .0e", 0.0);
+  TEST_OUTPUT("+0e+00", "%+.0e", 0.0);
 }
 
 TEST(snprintf, float10_e_pi) {
-  char buf[107];
-  ASSERT_EQ(5, snprintf(buf, sizeof(buf), "%.0e", M_PI));
-  ASSERT_STREQ("3e+00", buf);
-  ASSERT_EQ(7, snprintf(buf, sizeof(buf), "%.1e", M_PI));
-  ASSERT_STREQ("3.1e+00", buf);
-  ASSERT_EQ(8, snprintf(buf, sizeof(buf), "%.2e", M_PI));
-  ASSERT_STREQ("3.14e+00", buf);
-  ASSERT_EQ(9, snprintf(buf, sizeof(buf), "%.3e", M_PI));
-  ASSERT_STREQ("3.142e+00", buf);
-  ASSERT_EQ(10, snprintf(buf, sizeof(buf), "%.4e", M_PI));
-  ASSERT_STREQ("3.1416e+00", buf);
-  ASSERT_EQ(11, snprintf(buf, sizeof(buf), "%.5e", M_PI));
-  ASSERT_STREQ("3.14159e+00", buf);
-  ASSERT_EQ(12, snprintf(buf, sizeof(buf), "%.6e", M_PI));
-  ASSERT_STREQ("3.141593e+00", buf);
-  ASSERT_EQ(13, snprintf(buf, sizeof(buf), "%.7e", M_PI));
-  ASSERT_STREQ("3.1415927e+00", buf);
-  ASSERT_EQ(14, snprintf(buf, sizeof(buf), "%.8e", M_PI));
-  ASSERT_STREQ("3.14159265e+00", buf);
+  TEST_OUTPUT("3e+00", "%.0e", M_PI);
+  TEST_OUTPUT("3.1e+00", "%.1e", M_PI);
+  TEST_OUTPUT("3.14e+00", "%.2e", M_PI);
+  TEST_OUTPUT("3.142e+00", "%.3e", M_PI);
+  TEST_OUTPUT("3.1416e+00", "%.4e", M_PI);
+  TEST_OUTPUT("3.14159e+00", "%.5e", M_PI);
+  TEST_OUTPUT("3.141593e+00", "%.6e", M_PI);
+  TEST_OUTPUT("3.1415927e+00", "%.7e", M_PI);
+  TEST_OUTPUT("3.14159265e+00", "%.8e", M_PI);
 
 #if DBL_MANT_DIG == 53
   // This implementation already starts to return zeroes as soon as the
   // digits uniquely correspond with the floating point value.
-  ASSERT_EQ(106, snprintf(buf, sizeof(buf), "%.100e", M_PI));
-  ASSERT_STREQ(
+  TEST_OUTPUT(
       "3."
       "141592653589793000000000000000000000000000000000000000000000000000000000"
       "0000000000000000000000000000e+00",
-      buf);
+      "%.100e", M_PI);
 #endif
 }
 
 TEST(snprintf, float16_nan) {
-  char buf[11];
-  ASSERT_EQ(10, snprintf(buf, sizeof(buf), "%-10a", -NAN));
-  ASSERT_STREQ("-nan      ", buf);
-  ASSERT_EQ(10, snprintf(buf, sizeof(buf), "%10A", NAN));
-  ASSERT_STREQ("       NAN", buf);
+  TEST_OUTPUT("-nan      ", "%-10a", -NAN);
+  TEST_OUTPUT("       NAN", "%10A", NAN);
 }
 
 TEST(snprintf, float16_inf) {
-  char buf[11];
-  ASSERT_EQ(10, snprintf(buf, sizeof(buf), "%10a", INFINITY));
-  ASSERT_STREQ("       inf", buf);
-  ASSERT_EQ(10, snprintf(buf, sizeof(buf), "%-10A", -INFINITY));
-  ASSERT_STREQ("-INF      ", buf);
+  TEST_OUTPUT("       inf", "%10a", INFINITY);
+  TEST_OUTPUT("-INF      ", "%-10A", -INFINITY);
 }
 
 TEST(snprintf, float16_simple_zero) {
-  char buf[8];
-  ASSERT_EQ(6, snprintf(buf, sizeof(buf), "%a", 0.0));
-  ASSERT_STREQ("0x0p+0", buf);
-  ASSERT_EQ(7, snprintf(buf, sizeof(buf), "%#a", 0.0));
-  ASSERT_STREQ("0x0.p+0", buf);
+  TEST_OUTPUT("0x0p+0", "%a", 0.0);
+  TEST_OUTPUT("0x0.p+0", "%#a", 0.0);
 }
 
 TEST(snprintf, float16_simple_one) {
-  char buf[8];
-  ASSERT_EQ(6, snprintf(buf, sizeof(buf), "%a", 1.0));
-  ASSERT_STREQ("0x1p+0", buf);
-  ASSERT_EQ(7, snprintf(buf, sizeof(buf), "%#a", 1.0));
-  ASSERT_STREQ("0x1.p+0", buf);
+  TEST_OUTPUT("0x1p+0", "%a", 1.0);
+  TEST_OUTPUT("0x1.p+0", "%#a", 1.0);
 }
 
 TEST(snprintf, float16_simple_number) {
-  char buf[16];
-  ASSERT_EQ(15, snprintf(buf, sizeof(buf), "%a", 0x1234.5678p12));
-  ASSERT_STREQ("0x1.2345678p+24", buf);
+  TEST_OUTPUT("0x1.2345678p+24", "%a", 0x1234.5678p12);
 }
 
 TEST(snprintf, float16_negative) {
-  char buf[17];
-  ASSERT_EQ(16, snprintf(buf, sizeof(buf), "%a", -0x1234.5678p12));
-  ASSERT_STREQ("-0x1.2345678p+24", buf);
+  TEST_OUTPUT("-0x1.2345678p+24", "%a", -0x1234.5678p12);
 }
 
 TEST(snprintf, float16_round) {
-  char buf[21];
   ASSERT_EQ(0, fesetround(FE_DOWNWARD));
-  ASSERT_EQ(20, snprintf(buf, sizeof(buf), "%.2a %.2a", 1.51, -1.51));
-  ASSERT_STREQ("0x1.82p+0 -0x1.83p+0", buf);
+  TEST_OUTPUT("0x1.82p+0 -0x1.83p+0", "%.2a %.2a", 1.51, -1.51);
   ASSERT_EQ(0, fesetround(FE_TONEAREST));
-  ASSERT_EQ(20, snprintf(buf, sizeof(buf), "%.2a %.2a", 1.51, -1.51));
-  ASSERT_STREQ("0x1.83p+0 -0x1.83p+0", buf);
+  TEST_OUTPUT("0x1.83p+0 -0x1.83p+0", "%.2a %.2a", 1.51, -1.51);
   ASSERT_EQ(0, fesetround(FE_TOWARDZERO));
-  ASSERT_EQ(20, snprintf(buf, sizeof(buf), "%.2a %.2a", 1.51, -1.51));
-  ASSERT_STREQ("0x1.82p+0 -0x1.82p+0", buf);
+  TEST_OUTPUT("0x1.82p+0 -0x1.82p+0", "%.2a %.2a", 1.51, -1.51);
   ASSERT_EQ(0, fesetround(FE_UPWARD));
-  ASSERT_EQ(20, snprintf(buf, sizeof(buf), "%.2a %.2a", 1.51, -1.51));
-  ASSERT_STREQ("0x1.83p+0 -0x1.82p+0", buf);
+  TEST_OUTPUT("0x1.83p+0 -0x1.82p+0", "%.2a %.2a", 1.51, -1.51);
 }
 
 TEST(snprintf, float16_align) {
-  char buf[31];
-  ASSERT_EQ(30, snprintf(buf, sizeof(buf), "%#30.15a", 0x12.abcp12));
-  ASSERT_STREQ("       0x1.2abc00000000000p+16", buf);
-  ASSERT_EQ(30, snprintf(buf, sizeof(buf), "%#-30.15A", 0x12.abcp12));
-  ASSERT_STREQ("0X1.2ABC00000000000P+16       ", buf);
-  ASSERT_EQ(30, snprintf(buf, sizeof(buf), "%#030.15a", 0x12.abcp12));
-  ASSERT_STREQ("0x00000001.2abc00000000000p+16", buf);
-  ASSERT_EQ(30, snprintf(buf, sizeof(buf), "%+#030.15a", 0x12.abcp12));
-  ASSERT_STREQ("+0x0000001.2abc00000000000p+16", buf);
-  ASSERT_EQ(30, snprintf(buf, sizeof(buf), "% #030.15a", 0x12.abcp12));
-  ASSERT_STREQ(" 0x0000001.2abc00000000000p+16", buf);
+  TEST_OUTPUT("       0x1.2abc00000000000p+16", "%#30.15a", 0x12.abcp12);
+  TEST_OUTPUT("0X1.2ABC00000000000P+16       ", "%#-30.15A", 0x12.abcp12);
+  TEST_OUTPUT("0x00000001.2abc00000000000p+16", "%#030.15a", 0x12.abcp12);
+  TEST_OUTPUT("+0x0000001.2abc00000000000p+16", "%+#030.15a", 0x12.abcp12);
+  TEST_OUTPUT(" 0x0000001.2abc00000000000p+16", "% #030.15a", 0x12.abcp12);
 }
 
 #if LDBL_HAS_SUBNORM == 1
 TEST(snprintf, float16_subnormal) {
 #if LDBL_MANT_DIG == 64
   // Print subnormal values.
-  char buf[29];
-  ASSERT_EQ(10, snprintf(buf, sizeof(buf), "%La", LDBL_TRUE_MIN));
-  ASSERT_STREQ("0x1p-16445", buf);
+  TEST_OUTPUT("0x1p-16445", "%La", LDBL_TRUE_MIN);
   long double high = nexttowardl(LDBL_MIN, 0.0L);
-  ASSERT_EQ(27, snprintf(buf, sizeof(buf), "%La", high));
-  ASSERT_STREQ("0x1.fffffffffffffffcp-16383", buf);
+  TEST_OUTPUT("0x1.fffffffffffffffcp-16383", "%La", high);
 #elif LDBL_MANT_DIG == 113
-  char buf[40];
-  ASSERT_EQ(10, snprintf(buf, sizeof(buf), "%La", LDBL_TRUE_MIN));
-  ASSERT_STREQ("0x1p-16494", buf);
+  TEST_OUTPUT("0x1p-16494", "0x1p-16494");
   long double high = nexttowardl(LDBL_MIN, 0.0L);
-  ASSERT_EQ(39, snprintf(buf, sizeof(buf), "%La", high));
-  ASSERT_STREQ("0x1.fffffffffffffffffffffffffffep-16383", buf);
+  TEST_OUTPUT("0x1.fffffffffffffffffffffffffffep-16383", "%La", high);
 #else
 #error "Unknown floating point type"
 #endif
 
   // Test rounding behaviour of subnormal values.
   ASSERT_EQ(0, fesetround(FE_DOWNWARD));
-  ASSERT_EQ(28, snprintf(buf, sizeof(buf), "%.2La %.2La", high, -high));
-  ASSERT_STREQ("0x1.ffp-16383 -0x1.00p-16382", buf);
+  TEST_OUTPUT("0x1.ffp-16383 -0x1.00p-16382", "%.2La %.2La", high, -high);
   ASSERT_EQ(0, fesetround(FE_TONEAREST));
-  ASSERT_EQ(28, snprintf(buf, sizeof(buf), "%.2La %.2La", high, -high));
-  ASSERT_STREQ("0x1.00p-16382 -0x1.00p-16382", buf);
+  TEST_OUTPUT("0x1.00p-16382 -0x1.00p-16382", "%.2La %.2La", high, -high);
   ASSERT_EQ(0, fesetround(FE_TOWARDZERO));
-  ASSERT_EQ(28, snprintf(buf, sizeof(buf), "%.2La %.2La", high, -high));
-  ASSERT_STREQ("0x1.ffp-16383 -0x1.ffp-16383", buf);
+  TEST_OUTPUT("0x1.ffp-16383 -0x1.ffp-16383", "%.2La %.2La", high, -high);
   ASSERT_EQ(0, fesetround(FE_UPWARD));
-  ASSERT_EQ(28, snprintf(buf, sizeof(buf), "%.2La %.2La", high, -high));
-  ASSERT_STREQ("0x1.00p-16382 -0x1.ffp-16383", buf);
+  TEST_OUTPUT("0x1.00p-16382 -0x1.ffp-16383", "%.2La %.2La", high, -high);
 }
 #endif
