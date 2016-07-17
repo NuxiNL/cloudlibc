62cfc59790b01234d64584f76d4dadda8c134a71
Ed Schouten <ed@nuxi.nl>
2016-07-17 13:10:28 +0200

    Make LC_MESSAGES for the English locale complete.
    Only the signal descriptions were missing. These can be added trivially.

--- src/libc/string/strsignal_test.c
+++ src/libc/string/strsignal_test.c
@@ -8,14 +8,40 @@
 #include <string.h>
 #include <testing.h>
 
-TEST(strsignal, unknown) {
-  errno = 1234;
-  ASSERT_STREQ("Unknown signal", strsignal(-1));
-  ASSERT_EQ(1234, errno);
-}
+TEST(strsignal, examples) {
+#define TEST_SIGNAL(code, str)          \
+  do {                                  \
+    errno = 1234;                       \
+    ASSERT_STREQ(str, strsignal(code)); \
+    ASSERT_EQ(1234, errno);             \
+  } while (0)
+  TEST_SIGNAL(-1, "Unknown signal");
+  TEST_SIGNAL(0, "Unknown signal");
 
-TEST(strsignal, sigabrt) {
-  errno = 1234;
-  ASSERT_STREQ("Process abort signal", strsignal(SIGABRT));
-  ASSERT_EQ(1234, errno);
+  TEST_SIGNAL(SIGABRT, "Abort trap");
+  TEST_SIGNAL(SIGALRM, "Alarm clock");
+  TEST_SIGNAL(SIGBUS, "Bus error");
+  TEST_SIGNAL(SIGCHLD, "Child exited");
+  TEST_SIGNAL(SIGCONT, "Continued");
+  TEST_SIGNAL(SIGFPE, "Floating point exception");
+  TEST_SIGNAL(SIGHUP, "Hangup");
+  TEST_SIGNAL(SIGILL, "Illegal instruction");
+  TEST_SIGNAL(SIGINT, "Interrupt");
+  TEST_SIGNAL(SIGKILL, "Killed");
+  TEST_SIGNAL(SIGPIPE, "Broken pipe");
+  TEST_SIGNAL(SIGQUIT, "Quit");
+  TEST_SIGNAL(SIGSEGV, "Segmentation fault");
+  TEST_SIGNAL(SIGSTOP, "Suspended (signal)");
+  TEST_SIGNAL(SIGSYS, "Bad system call");
+  TEST_SIGNAL(SIGTERM, "Terminated");
+  TEST_SIGNAL(SIGTRAP, "Trace/BPT trap");
+  TEST_SIGNAL(SIGTSTP, "Suspended");
+  TEST_SIGNAL(SIGTTIN, "Stopped (tty input)");
+  TEST_SIGNAL(SIGTTOU, "Stopped (tty output)");
+  TEST_SIGNAL(SIGURG, "Urgent I/O condition");
+  TEST_SIGNAL(SIGUSR1, "User defined signal 1");
+  TEST_SIGNAL(SIGUSR2, "User defined signal 2");
+  TEST_SIGNAL(SIGVTALRM, "Virtual timer expired");
+  TEST_SIGNAL(SIGXCPU, "Cputime limit exceeded");
+  TEST_SIGNAL(SIGXFSZ, "Filesize limit exceeded");
 }
