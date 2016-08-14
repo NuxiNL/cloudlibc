594c26f809457f8b0463c9759b16f26d3cf6521d
Ed Schouten <ed@nuxi.nl>
2016-08-14 20:48:23 +0200

    Let program_exec() use mmap() to allocate memory.
    Using a VLA here is unwise, as the argument data could potentially be
    pretty large. By using mmap(), the size is effectively unrestricted.

--- src/libc/program/program_exec.c
+++ src/libc/program/program_exec.c
@@ -8,15 +8,30 @@
 #include <program.h>
 
 int program_exec(int fd, const argdata_t *ad) {
-  // Convert argument data to binary format.
+  // Compute size needed for serializing the argument data.
   size_t datalen;
   size_t fdslen;
   argdata_get_buffer_length(ad, &datalen, &fdslen);
-  char data[datalen];
-  int fds[fdslen];
+
+  // Allocate space for the serialized the argument data.
+  size_t mappinglen = fdslen * sizeof(int) + datalen;
+  void *mapping;
+  cloudabi_errno_t error = cloudabi_sys_mem_map(
+      NULL, mappinglen, CLOUDABI_PROT_READ | CLOUDABI_PROT_WRITE,
+      CLOUDABI_MAP_ANON | CLOUDABI_MAP_PRIVATE, CLOUDABI_MAP_ANON_FD, 0,
+      &mapping);
+  if (error != 0)
+    return error;
+
+  // Serialize the argument data.
+  int *fds = mapping;
+  char *data = (char *)&fds[fdslen];
   fdslen = argdata_get_buffer(ad, data, fds);
 
-  // Invoke system call with binary data and an array of file descriptors.
-  return cloudabi_sys_proc_exec(fd, data, datalen, (cloudabi_fd_t *)fds,
-                                fdslen);
+  // Execute the new process, providing it the serialized argument data
+  // and a list of file descriptors.
+  error =
+      cloudabi_sys_proc_exec(fd, data, datalen, (cloudabi_fd_t *)fds, fdslen);
+  cloudabi_sys_mem_unmap(mapping, mappinglen);
+  return error;
 }
