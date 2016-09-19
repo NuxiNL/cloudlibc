a41d4e91b4cf9f030d782cc5a71770015709370f
Ed Schouten <ed@nuxi.nl>
2016-09-19 21:25:43 +0200

    Implement setjmp() and longjmp().

--- src/include/_/arch/arm/setjmp.h
+++ src/include/_/arch/arm/setjmp.h
@@ -24,9 +24,34 @@
 #ifndef ___ARCH_ARM_SETJMP_H_
 #define ___ARCH_ARM_SETJMP_H_
 
-// TODO(ed): Implement.
+#include <_/types.h>
+
+// Registers that need to be preserved across function calls, per the
+// Procedure Call Standard for the ARM Architecture, section 5.1.1,
+// table 2, and section 5.1.2.
 struct __jmp_buf {
-  int __bogus;
+  __uint64_t __d8;   // Callee-saved VFP register.
+  __uint64_t __d9;   // Callee-saved VFP register.
+  __uint64_t __d10;  // Callee-saved VFP register.
+  __uint64_t __d11;  // Callee-saved VFP register.
+  __uint64_t __d12;  // Callee-saved VFP register.
+  __uint64_t __d13;  // Callee-saved VFP register.
+  __uint64_t __d14;  // Callee-saved VFP register.
+  __uint64_t __d15;  // Callee-saved VFP register.
+
+  __uint32_t __r4;  // Callee-saved register.
+  __uint32_t __r5;  // Callee-saved register.
+  __uint32_t __r6;  // Callee-saved register.
+  __uint32_t __r7;  // Callee-saved register.
+  __uint32_t __r8;  // Callee-saved register.
+  __uint32_t __r9;  // Callee-saved register.
+  __uint32_t __r10;  // Callee-saved register.
+  __uint32_t __r11;  // Callee-saved register.
+  __uint32_t __r12;  // The Intra-Procedure-call scratch register.
+  __uint32_t __r13;  // The Stack Pointer.
+  __uint32_t __r14;  // The Link Register.
+
 };
+_Static_assert(sizeof(struct __jmp_buf) == 112, "ABI breakage");
 
 #endif
