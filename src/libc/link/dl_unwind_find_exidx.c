8ddb7776cc2051c80d39655b89fe7afbb61382e8
Ed Schouten <ed@nuxi.nl>
2016-09-29 22:38:08 +0200

    Make the implementation of dl_unwind_find_exidx() complete.
    Exceptions now seem to work on ARMv6 properly.

--- src/libc/link/dl_unwind_find_exidx.c
+++ src/libc/link/dl_unwind_find_exidx.c
@@ -3,13 +3,24 @@
 // This file is distributed under a 2-clause BSD license.
 // See the LICENSE file for details.
 
+#include <common/crt.h>
+
 #include <link.h>
 #include <stddef.h>
 
 #ifdef __arm__
 
 void *dl_unwind_find_exidx(const void *pc, int *pcount) {
-  // TODO(ed): Implement.
+  // Extract exception information from PT_ARM_EXIDX.
+  // TODO(ed): Why does this need to be a libc function? Can't libunwind
+  // just use dl_iterate_phdr() like it does on other architectures?
+  for (ElfW(Half) i = 0; i < __at_phnum; ++i) {
+    const ElfW(Phdr) *phdr = &__at_phdr[i];
+    if (phdr->p_type == PT_ARM_EXIDX) {
+      *pcount = phdr->p_memsz / (2 * sizeof(void *));
+      return (char *)__at_base + phdr->p_vaddr;
+    }
+  }
   *pcount = 0;
   return NULL;
 }
