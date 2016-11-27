623e9c3d615a72e6d8218ec98878f7af360a821f
Ed Schouten <ed@nuxi.nl>
2016-11-27 21:36:02 +0100

    Decompose and document the sorting algorithm more thoroughly.

--- src/include/stdlib.h
+++ src/include/stdlib.h
@@ -277,20 +277,38 @@ static __inline void *__bsearch(const void *__key, const void *__base,
 // size of the elements into account. It also reduces the overhead of
 // indirect function calls.
 
+static __inline void __qsort_r(void *, size_t, size_t,
+                               int (*)(const void *, const void *, void *),
+                               void *);
+
 static __inline size_t __qsort_min(size_t __a, size_t __b) {
   return __a < __b ? __a : __b;
 }
 
+// Swaps the contents of two buffers.
 static __inline void __qsort_swap(char *__a, char *__b, size_t __n) {
-  char t;
+  char __t;
 
   while (__n-- > 0) {
-    t = *__a;
+    __t = *__a;
     *__a++ = *__b;
-    *__b++ = t;
+    *__b++ = __t;
   }
 }
 
+// Implementation of insertionsort for small lists.
+static __inline void __qsort_insertionsort(
+    char *__a, size_t __nel, size_t __width,
+    int (*__cmp)(const void *, const void *, void *), void *__thunk) {
+  char *__pm, *__pl;
+
+  for (__pm = __a + __width; __pm < __a + __nel * __width; __pm += __width)
+    for (__pl = __pm; __pl > __a && __cmp(__pl - __width, __pl, __thunk) > 0;
+         __pl -= __width)
+      __qsort_swap(__pl, __pl - __width, __width);
+}
+
+// Returns the median of three elements.
 static __inline char *__qsort_med3(char *__a, char *__b, char *__c,
                                    int (*__cmp)(const void *, const void *,
                                                 void *),
@@ -304,21 +322,15 @@ static __inline char *__qsort_med3(char *__a, char *__b, char *__c,
                     : __cmp(__a, __c, __thunk) > 0 ? __c : __a);
 }
 
-static __inline void __qsort_r(void *__base, size_t __nel, size_t __width,
-                               int (*__cmp)(const void *, const void *, void *),
-                               void *__thunk) {
-  char *__a, *__pa, *__pb, *__pc, *__pd, *__pl, *__pm, *__pn, *__pv;
-  int __r;
+// Determines which element should act as a pivot for quick sort.
+// TODO(ed): This does not guarantee a O(n log n) running time.
+static __inline char *__qsort_pickpivot(char *__a, size_t __nel, size_t __width,
+                                        int (*__cmp)(const void *, const void *,
+                                                     void *),
+                                        void *__thunk) {
+  char *__pl, *__pm, *__pn;
   size_t __s;
 
-  __a = (char *)__base;
-  if (__nel < 7) {
-    for (__pm = __a + __width; __pm < __a + __nel * __width; __pm += __width)
-      for (__pl = __pm; __pl > __a && __cmp(__pl - __width, __pl, __thunk) > 0;
-           __pl -= __width)
-        __qsort_swap(__pl, __pl - __width, __width);
-    return;
-  }
   __pm = __a + (__nel / 2) * __width;
   if (__nel > 7) {
     __pl = __a;
@@ -331,19 +343,34 @@ static __inline void __qsort_r(void *__base, size_t __nel, size_t __width,
     }
     __pm = __qsort_med3(__pl, __pm, __pn, __cmp, __thunk);
   }
-  __pv = __a;
-  __qsort_swap(__pv, __pm, __width);
+  return __pm;
+}
+
+// Implementation of quicksort for larger lists.
+static __inline void __qsort_quicksort(char *__a, size_t __nel, size_t __width,
+                                       int (*__cmp)(const void *, const void *,
+                                                    void *),
+                                       void *__thunk) {
+  char *__pa, *__pb, *__pc, *__pd, *__pn;
+  int __r;
+  size_t __s;
+
+  // Select pivot and move it to the head of the list.
+  __qsort_swap(__a, __qsort_pickpivot(__a, __nel, __width, __cmp, __thunk),
+               __width);
+
+  // Perform partitioning.
   __pa = __pb = __a;
   __pc = __pd = __a + (__nel - 1) * __width;
   for (;;) {
-    while (__pb <= __pc && (__r = __cmp(__pb, __pv, __thunk)) <= 0) {
+    while (__pb <= __pc && (__r = __cmp(__pb, __a, __thunk)) <= 0) {
       if (__r == 0) {
         __qsort_swap(__pa, __pb, __width);
         __pa += __width;
       }
       __pb += __width;
     }
-    while (__pc >= __pb && (__r = __cmp(__pc, __pv, __thunk)) >= 0) {
+    while (__pc >= __pb && (__r = __cmp(__pc, __a, __thunk)) >= 0) {
       if (__r == 0) {
         __qsort_swap(__pc, __pd, __width);
         __pd -= __width;
@@ -356,19 +383,32 @@ static __inline void __qsort_r(void *__base, size_t __nel, size_t __width,
     __pb += __width;
     __pc -= __width;
   }
+
+  // Store pivot between the two partitions.
   __pn = __a + __nel * __width;
   __s = __qsort_min((size_t)(__pa - __a), (size_t)(__pb - __pa));
-  if (__s > 0)
-    __qsort_swap(__a, __pb - __s, __s);
+  __qsort_swap(__a, __pb - __s, __s);
   __s = __qsort_min((size_t)(__pd - __pc), (size_t)(__pn - __pd) - __width);
-  if (__s > 0)
-    __qsort_swap(__pb, __pn - __s, __s);
+  __qsort_swap(__pb, __pn - __s, __s);
+
+  // Sort the two partitions.
   __s = (size_t)(__pb - __pa);
-  if (__s > __width)
-    __qsort_r(__a, __s / __width, __width, __cmp, __thunk);
+  __qsort_r(__a, __s / __width, __width, __cmp, __thunk);
   __s = (size_t)(__pd - __pc);
-  if (__s > __width)
-    __qsort_r(__pn - __s, __s / __width, __width, __cmp, __thunk);
+  __qsort_r(__pn - __s, __s / __width, __width, __cmp, __thunk);
+}
+
+static __inline void __qsort_r(void *__base, size_t __nel, size_t __width,
+                               int (*__cmp)(const void *, const void *, void *),
+                               void *__thunk) {
+  char *__a;
+
+  __a = (char *)__base;
+  if (__nel < 7) {
+    __qsort_insertionsort(__a, __nel, __width, __cmp, __thunk);
+  } else {
+    __qsort_quicksort(__a, __nel, __width, __cmp, __thunk);
+  }
 }
 #define qsort_r(base, nel, width, compar, thunk) \
   __qsort_r(base, nel, width, compar, thunk)
