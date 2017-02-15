64b626f66a410468e3e58600b1d1d627cbb09046
Ed Schouten <ed@nuxi.nl>
2017-02-15 11:18:49 +0100

    Make the dladdr() tests work on non-PIE systems.
    On non-PIE systems, the dli_fbase field may contain the value zero.
    Change the test to only require the value to be page-aligned, like we do
    for dl_iterate_phdr().

--- src/libc/dlfcn/dladdr_test.c
+++ src/libc/dlfcn/dladdr_test.c
@@ -5,9 +5,11 @@
 
 #include <dlfcn.h>
 #include <errno.h>
+#include <stdint.h>
 #include <string.h>
 #include <testing.h>
 #include <time.h>
+#include <unistd.h>
 
 TEST(dladdr, nonexistent) {
   ASSERT_FALSE(dladdr(NULL, NULL));
@@ -17,7 +19,7 @@ TEST(dladdr, global_function) {
   Dl_info info;
   ASSERT_TRUE(dladdr(strlen, &info));
   ASSERT_STREQ("unknown", info.dli_fname);
-  ASSERT_NE(NULL, info.dli_fbase);
+  ASSERT_EQ(0x0, (uintptr_t)info.dli_fbase % sysconf(_SC_PAGESIZE));
   ASSERT_STREQ("strlen", info.dli_sname);
   ASSERT_EQ(strlen, info.dli_saddr);
   ASSERT_LT(info.dli_fbase, info.dli_saddr);
@@ -28,14 +30,14 @@ TEST(dladdr, global_variable) {
   Dl_info info;
   ASSERT_TRUE(dladdr(&tzname[0], &info));
   ASSERT_STREQ("unknown", info.dli_fname);
-  ASSERT_NE(NULL, info.dli_fbase);
+  ASSERT_EQ(0x0, (uintptr_t)info.dli_fbase % sysconf(_SC_PAGESIZE));
   ASSERT_STREQ("tzname", info.dli_sname);
   ASSERT_EQ(tzname, info.dli_saddr);
   ASSERT_LT(info.dli_fbase, info.dli_saddr);
 
   ASSERT_TRUE(dladdr(&tzname[1], &info));
   ASSERT_STREQ("unknown", info.dli_fname);
-  ASSERT_NE(NULL, info.dli_fbase);
+  ASSERT_EQ(0x0, (uintptr_t)info.dli_fbase % sysconf(_SC_PAGESIZE));
   ASSERT_STREQ("tzname", info.dli_sname);
   ASSERT_EQ(tzname, info.dli_saddr);
   ASSERT_LT(info.dli_fbase, info.dli_saddr);
@@ -45,7 +47,7 @@ TEST(dladdr, tls_variable) {
   Dl_info info;
   ASSERT_TRUE(dladdr(&errno, &info));
   ASSERT_STREQ("unknown", info.dli_fname);
-  ASSERT_NE(NULL, info.dli_fbase);
+  ASSERT_EQ(0x0, (uintptr_t)info.dli_fbase % sysconf(_SC_PAGESIZE));
   ASSERT_STREQ("errno", info.dli_sname);
   ASSERT_EQ(&errno, info.dli_saddr);
 }
