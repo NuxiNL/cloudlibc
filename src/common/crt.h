098f3c75009a972c9d9b4afaf40e13a6580885fa
Ed Schouten <ed@nuxi.nl>
2017-02-24 20:14:35 +0100

    Bump copyright years on modified source files.

--- src/common/crt.h
+++ src/common/crt.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2015-2016 Nuxi, https://nuxi.nl/
+// Copyright (c) 2015-2017 Nuxi, https://nuxi.nl/
 //
 // This file is distributed under a 2-clause BSD license.
 // See the LICENSE file for details.
