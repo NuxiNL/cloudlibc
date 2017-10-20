ab13fd582e04e99b2d5c3c4b1e5013fe73bb16aa
Ed Schouten <ed@nuxi.nl>
2017-10-20 13:31:46 +0200

    Properly implement uv_print_active_handles().

--- src/libc/uv/uv_print_active_handles.c
+++ src/libc/uv/uv_print_active_handles.c
@@ -5,6 +5,16 @@
 
 #include <uv.h>
 
+#include "uv_impl.h"
+
 void uv_print_active_handles(uv_loop_t *loop, FILE *stream) {
-  // TODO(ed): Implement!
+  struct __uv_handles_head handles;
+  __uv_handles_move(&loop->__handles, &handles);
+  while (!__uv_handles_empty(&handles)) {
+    uv_handle_t *handle = __uv_handles_first(&handles);
+    __uv_handles_remove(handle);
+    __uv_handles_insert_last(&loop->__handles, handle);
+    if (uv_is_active(handle))
+      __uv_print_handle(handle, stream);
+  }
 }
