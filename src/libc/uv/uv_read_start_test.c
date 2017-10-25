39c18fb3a8cfada9a0de4707bd7a58984a168d18
Ed Schouten <ed@nuxi.nl>
2017-10-25 11:17:56 +0200

    Extend the stream reading test to see what happens when passing fds.

--- src/libc/uv/uv_read_start_test.c
+++ src/libc/uv/uv_read_start_test.c
@@ -3,7 +3,11 @@
 // This file is distributed under a 2-clause BSD license.
 // See the LICENSE file for details.
 
+#include <sys/socket.h>
+
+#include <stdalign.h>
 #include <stdlib.h>
+#include <string.h>
 #include <testing.h>
 #include <unistd.h>
 #include <uv.h>
@@ -42,12 +46,12 @@ static void read_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf) {
 static void close_cb(uv_handle_t *handle) {
 }
 
-TEST(uv_read_start, example) {
+TEST(uv_read_start, ipc_disabled) {
   uv_loop_t loop;
   ASSERT_EQ(0, uv_loop_init(&loop));
 
   int fds[2];
-  ASSERT_EQ(0, pipe(fds));
+  ASSERT_EQ(0, socketpair(AF_UNIX, SOCK_STREAM, 0, fds));
   uv_pipe_t pipe;
   ASSERT_EQ(0, uv_pipe_init(&loop, &pipe, 0));
   ASSERT_EQ(0, uv_pipe_open(&pipe, fds[0]));
@@ -62,34 +66,64 @@ TEST(uv_read_start, example) {
   ASSERT_EQ(0, state.bytes);
   ASSERT_EQ(0, state.eofs);
 
-  // Write less than a single buffer of data.
-  ASSERT_EQ(5, write(fds[1], "ABCDE", 5));
+  // Perform multiple writes that in total are smaller than the read
+  // buffer size. Any file descriptors attached to the payload should be
+  // discarded, as the stream has IPC disabled. Still, there should be
+  // artificial read boundaries introduced by where the file descriptors
+  // were.
+  ASSERT_EQ(1, write(fds[1], "A", 1));
+  {
+    struct iovec iov = {.iov_base = (char *)"BC", .iov_len = 2};
+    alignas(struct cmsghdr) char control[CMSG_SPACE(sizeof(fds))];
+    struct msghdr msg = {
+      .msg_iov = &iov,
+      .msg_iovlen = 1,
+      .msg_control = control,
+      .msg_controllen = sizeof(control),
+    };
+    struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msg);
+    cmsg->cmsg_len = CMSG_LEN(sizeof(fds));
+    cmsg->cmsg_level = SOL_SOCKET;
+    cmsg->cmsg_type = SCM_RIGHTS;
+    memcpy(CMSG_DATA(cmsg), fds, sizeof(fds));
+    ASSERT_EQ(2, sendmsg(fds[1], &msg, 0));
+  }
+  ASSERT_EQ(2, write(fds[1], "DE", 2));
   ASSERT_EQ(0, uv_read_start((uv_stream_t *)&pipe, alloc_cb, read_cb));
   ASSERT_EQ(1, uv_run(&loop, UV_RUN_ONCE));
   ASSERT_EQ(1, state.allocs);
   ASSERT_EQ(1, state.reads);
+  ASSERT_EQ(1, state.bytes);
+  ASSERT_EQ(0, state.eofs);
+  ASSERT_EQ(0, uv_pipe_pending_count(&pipe));
+  ASSERT_EQ(1, uv_run(&loop, UV_RUN_ONCE));
+  ASSERT_EQ(2, state.allocs);
+  ASSERT_EQ(2, state.reads);
   ASSERT_EQ(5, state.bytes);
   ASSERT_EQ(0, state.eofs);
+  ASSERT_EQ(0, uv_pipe_pending_count(&pipe));
 
   // Write more than a single buffer of data. This should be split up in
   // multiple reads.
   ASSERT_EQ(16, write(fds[1], "FGHIJKLMNOPQRSTU", 16));
   ASSERT_EQ(0, uv_read_start((uv_stream_t *)&pipe, alloc_cb, read_cb));
   ASSERT_EQ(1, uv_run(&loop, UV_RUN_ONCE));
-  ASSERT_EQ(3, state.allocs);
-  ASSERT_EQ(3, state.reads);
+  ASSERT_EQ(4, state.allocs);
+  ASSERT_EQ(4, state.reads);
   ASSERT_EQ(21, state.bytes);
   ASSERT_EQ(0, state.eofs);
+  ASSERT_EQ(0, uv_pipe_pending_count(&pipe));
 
   // Terminate the input. The loop should now stop, as there is nothing
   // left to do.
   ASSERT_EQ(5, write(fds[1], "VWXYZ", 5));
   ASSERT_EQ(0, close(fds[1]));
   ASSERT_EQ(0, uv_run(&loop, UV_RUN_DEFAULT));
-  ASSERT_EQ(5, state.allocs);
-  ASSERT_EQ(5, state.reads);
+  ASSERT_EQ(6, state.allocs);
+  ASSERT_EQ(6, state.reads);
   ASSERT_EQ(26, state.bytes);
   ASSERT_EQ(1, state.eofs);
+  ASSERT_EQ(0, uv_pipe_pending_count(&pipe));
 
   uv_close((uv_handle_t *)&pipe, close_cb);
   ASSERT_EQ(0, uv_run(&loop, UV_RUN_DEFAULT));
