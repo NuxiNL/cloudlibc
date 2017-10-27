eb50d955f0d82e57ce748f551f6c52871f10b94c
Ed Schouten <ed@nuxi.nl>
2017-10-27 12:59:22 +0200

    Bump the version of cloudlibc to 0.90.
    We're going to cut another release that includes all of the bits
    necessary to build Node.js.

--- src/include/_/cdefs.h
+++ src/include/_/cdefs.h
@@ -27,7 +27,7 @@
 // Version information.
 #define __cloudlibc__ 1
 #define __cloudlibc_major__ 0
-#define __cloudlibc_minor__ 89
+#define __cloudlibc_minor__ 90
 
 #ifdef __cplusplus
 #define __BEGIN_DECLS extern "C" {
