386e9dafe33b1caec9e5658761a7b94ea8097030
Ed Schouten <ed@nuxi.nl>
2017-11-01 10:26:36 +0100

    Release 0.92.
    Changes include the addition of uname() and uv_tty_t to make Node.js
    build more cleanly.

--- src/include/_/cdefs.h
+++ src/include/_/cdefs.h
@@ -27,7 +27,7 @@
 // Version information.
 #define __cloudlibc__ 1
 #define __cloudlibc_major__ 0
-#define __cloudlibc_minor__ 91
+#define __cloudlibc_minor__ 92
 
 #ifdef __cplusplus
 #define __BEGIN_DECLS extern "C" {
