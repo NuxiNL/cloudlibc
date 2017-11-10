45350a29f0bc619be1209639c573200982ed1d4e
Ed Schouten <ed@nuxi.nl>
2017-11-10 07:59:08 +0100

    Don't crash in case __at_pid is NULL.
    In my opinion all runtime environments SHOULD provide this field in the
    auxiliary vector. Still, that doesn't mean we should crash if it's not
    provided.
    
    Reported by: Sjors Gielen

--- src/libc/program/program_getpid.c
+++ src/libc/program/program_getpid.c
@@ -12,6 +12,11 @@
 static char uuid[sizeof("e2a38354-c462-11e7-ae9b-d43d7ef8eef2")];
 
 static void generate(void) {
+  // Don't crash if the environment did not provide a PID. This will
+  // leave the UUID empty, which is obviously incorrect.
+  if (__at_pid == NULL)
+    return;
+
   // Convert the UUID provided by the environment to a string.
   char *out = uuid;
   for (size_t i = 0; i < 16; ++i) {
