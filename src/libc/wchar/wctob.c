3d268f7cf6989e25014a21bf806082549238c491
Ed Schouten <ed@nuxi.nl>
2017-12-05 08:43:06 +0100

    Suppress compiler warnings when wint_t is unsigned.
    In this case it makes little sense to test it for being negative. Use
    WINT_MIN to determine whether we should do this test.

--- src/libc/wchar/wctob.c
+++ src/libc/wchar/wctob.c
@@ -3,11 +3,16 @@
 // SPDX-License-Identifier: BSD-2-Clause
 
 #include <limits.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <wchar.h>
 
 int wctob(wint_t c) {
-  if (c < 0 || c > SCHAR_MAX)
+  if (
+#if WINT_MIN < 0
+      c < 0 ||
+#endif
+      c > SCHAR_MAX)
     return EOF;
   return c;
 }
--- src/libc/wctype/iswcntrl.c
+++ src/libc/wctype/iswcntrl.c
@@ -2,9 +2,14 @@
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
+#include <stdint.h>
 #include <wctype.h>
 
 int iswcntrl(wint_t wc) {
-  return (wc >= 0x0 && wc <= 0x1f) || (wc >= 0x7f && wc <= 0x9f) ||
-         (wc >= 0x2028 && wc <= 0x2029);
+  return (
+#if WINT_MIN < 0
+             wc >= 0x0 &&
+#endif
+             wc <= 0x1f) ||
+         (wc >= 0x7f && wc <= 0x9f) || (wc >= 0x2028 && wc <= 0x2029);
 }
