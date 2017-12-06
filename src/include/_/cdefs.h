d72fc4e272f5e13d93034d237d376eae56557687
Ed Schouten <ed@nuxi.nl>
2017-12-06 11:22:39 +0100

    Bump the version of cloudlibc.
    This should have been part of the previous release...

--- src/include/_/cdefs.h
+++ src/include/_/cdefs.h
@@ -27,7 +27,7 @@
 // Version information.
 #define __cloudlibc__ 1
 #define __cloudlibc_major__ 0
-#define __cloudlibc_minor__ 94
+#define __cloudlibc_minor__ 95
 
 #ifdef __cplusplus
 #define __BEGIN_DECLS extern "C" {
