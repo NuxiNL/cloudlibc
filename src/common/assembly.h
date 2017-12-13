df5d09f7dd2144c3966db0f7687e8d13e29b7038
Ed Schouten <ed@nuxi.nl>
2017-12-13 12:38:00 +0100

    Remove assembly macros for non-ARM systems.
    We no longer build any assembly for non-ARM systems. No longer making
    these definitions required makes it easier to build for RISC-V.

--- src/common/assembly.h
+++ src/common/assembly.h
@@ -1,58 +1,20 @@
-// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+// Copyright (c) 2015-2017 Nuxi, https://nuxi.nl/
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
 #ifndef COMMON_ASSEMBLY_H
 #define COMMON_ASSEMBLY_H
 
-#if defined(__aarch64__)
+#if defined(__arm__)
 
 #define ENTRY(name) \
   .text;            \
   .align 2;         \
   .global name;     \
   name:
-
+#define ALTENTRY ENTRY
 #define END(name) .size name, .- name
-
-#elif defined(__arm__)
-
-#define ENTRY(name) \
-  .text;            \
-  .align 2;         \
-  .global name;     \
-  name:
-
-#define END(name) .size name, .- name
-
-#elif defined(__i386__)
-
-#define ENTRY(name)      \
-  .text;                 \
-  .p2align 2, 0x90;      \
-  .global name;          \
-  .type name, @function; \
-  name:
-
-#define END(name) .size name, .- name
-
-#elif defined(__x86_64__)
-
-#define ENTRY(name)      \
-  .text;                 \
-  .p2align 4, 0x90;      \
-  .global name;          \
-  .type name, @function; \
-  name:
-
-#define END(name) .size name, .- name
-
-#else
-
-#error "Assembly macros not implemented for this architecture"
 
 #endif
-
-#define ALTENTRY ENTRY
 
 #endif
