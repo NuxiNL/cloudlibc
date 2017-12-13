2197afe5fe03c5cb80592e5a8167f8f8cb1292be
Ed Schouten <ed@nuxi.nl>
2017-12-13 12:01:11 +0100

    Make the setjmp()/sigsetjmp() examples comply to the C standard.
    We're only allowed to use this function in certain contects, such as
    if/switch statements.

--- src/libc/setjmp/setjmp_longjmp_test.c
+++ src/libc/setjmp/setjmp_longjmp_test.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+// Copyright (c) 2015-2017 Nuxi, https://nuxi.nl/
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
@@ -6,15 +6,7 @@
 #include <testing.h>
 
 TEST(setjmp_longjmp, example) {
-  volatile int jumped = 0;
   jmp_buf jmp;
-  int ret = setjmp(jmp);
-  if (!jumped) {
-    ASSERT_EQ(0, ret);
-    jumped = 1;
+  while (setjmp(jmp) != 123)
     longjmp(jmp, 123);
-    ASSERT_TRUE(0);
-  } else {
-    ASSERT_EQ(123, ret);
-  }
 }
--- src/libc/setjmp/sigsetjmp_siglongjmp_test.c
+++ src/libc/setjmp/sigsetjmp_siglongjmp_test.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2015 Nuxi, https://nuxi.nl/
+// Copyright (c) 2015-2017 Nuxi, https://nuxi.nl/
 //
 // SPDX-License-Identifier: BSD-2-Clause
 
@@ -6,15 +6,7 @@
 #include <testing.h>
 
 TEST(sigsetjmp_siglongjmp, example) {
-  volatile int jumped = 0;
   sigjmp_buf jmp;
-  int ret = sigsetjmp(jmp, 0);
-  if (!jumped) {
-    ASSERT_EQ(0, ret);
-    jumped = 1;
+  while (sigsetjmp(jmp, 0) != 123)
     siglongjmp(jmp, 123);
-    ASSERT_TRUE(0);
-  } else {
-    ASSERT_EQ(123, ret);
-  }
 }
